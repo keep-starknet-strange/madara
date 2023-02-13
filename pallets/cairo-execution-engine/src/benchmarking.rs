5bd212e19db2d76868a28a7cc2d015afacace04c
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-02-13 09:51:08 +0100

    🚧 init cairo pallet benchmarking

--- pallets/cairo-execution-engine/src/benchmarking.rs
+++ pallets/cairo-execution-engine/src/benchmarking.rs
@@ -1,10 +1,33 @@
-//! Benchmarking setup for pallet-template
-
+//! Cairo Execution Engine pallet benchmarking.
+#![cfg(feature = "runtime-benchmarks")]
 use super::*;
 
-#[allow(unused)]
-use crate::Pallet as Template;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use crate::Pallet as CairoExecutionEngine;
+use frame_benchmarking::{
+	account, benchmarks, whitelist_account, whitelisted_caller, BenchmarkError, Vec,
+};
+use frame_support::{
+	assert_noop, assert_ok,
+	sp_runtime::{traits::Bounded, BoundedVec},
+	sp_std,
+	traits::{Currency, EnsureOrigin, Get, OnInitialize, UnfilteredDispatchable},
+};
 use frame_system::RawOrigin;
 
-benchmarks! {}
+fn dummy_sierra_code<T: Config>() -> BoundedVec<u8, T::MaxSierraProgramLength> {
+	BoundedVec::truncate_from(sp_std::vec![1, 2, 3])
+}
+
+benchmarks! {
+	deploy_sierra_program {
+		let s in 0 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
+		let sierra_code = dummy_sierra_code::<T>();
+	}: _(RawOrigin::Signed(caller), sierra_code)
+	verify {
+		// TODO: Add post conditions checks.
+		assert_eq!(true, true);
+	}
+
+	impl_benchmark_test_suite!(CairoExecutionEngine, crate::mock::new_test_ext(), crate::mock::Test);
+}
--- pallets/starknet/src/lib.rs
+++ pallets/starknet/src/lib.rs
@@ -18,8 +18,6 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 
