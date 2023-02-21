7bc20b9f0ef2e703a45b9b3c282b45de10e396be
Evolve <Artevolve@yahoo.com>
2023-02-21 14:56:07 +0100

    feat: basic stress test 🚀
    - single user
    - fibonnaci program

--- benchmarking/scripts/functions.js
+++ benchmarking/scripts/functions.js
@@ -1,7 +1,8 @@
-// Copyright 2021-2022 Dwellir AB authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-module.exports = { rpcMethods, getHash, someComplexCall };
+const { Keyring } = require("@polkadot/keyring");
+
+module.exports = { rpcMethods, runCairoProgram };
 
 function rpcMethods(userContext, events, done) {
   const data = {"id":1, "jsonrpc":"2.0", "method": "rpc_methods"};
@@ -10,28 +11,17 @@ function rpcMethods(userContext, events, done) {
   return done();
 }
 
-function getHash(userContext, events, done) {
-  const { hash } = userContext.vars.data;
-
-  userContext.vars.data = hash;
-  return done();
-}
-
-async function someComplexCall(userContext, events, done) {
-  const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-  const [{ nonce: accountNonce }, now] = await Promise.all([
-   userContext.api.query.system.account(ALICE),
-   userContext.api.query.timestamp.now()
-  ]);
-
-  userContext.vars.accountNonce = accountNonce;
-  userContext.vars.now = now;
-  return done();
-}
-
 async function runCairoProgram(userContext, events, done) {
+  const { programId } = userContext.vars;
 
-  
+   const keyring = new Keyring({ type: "sr25519" });
+   const alice = keyring.addFromUri("//Alice");
+
+  const extrisinc = userContext.api.tx.cairo.executeHardcodedCairoAssemblyProgram(
+    programId
+  );
+  // console.log(extrisinc)
+  await extrisinc.signAndSend(alice, {nonce: -1})
 
   return done();
 }
