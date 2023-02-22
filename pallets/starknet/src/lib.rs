ff723a362d1497676cb064addabb2b57098ee6f0
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-02-22 10:45:13 +0100

    🚧 re-enable `starknet-crypto` in `kp-starknet`

--- pallets/starknet/src/lib.rs
+++ pallets/starknet/src/lib.rs
@@ -25,8 +25,8 @@ pub mod pallet {
 
 	use frame_support::{pallet_prelude::*, traits::Randomness};
 	use frame_system::pallet_prelude::*;
-	//use kp_starknet::crypto::hash;
-	//use starknet_crypto::FieldElement;
+	use kp_starknet::crypto::hash;
+	use starknet_crypto::FieldElement;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -49,7 +49,9 @@ pub mod pallet {
 	/// See: `<https://docs.substrate.io/main-docs/build/events-errors/>`
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
+	pub enum Event<T: Config> {
+		HealthCheckHashComputed { x: [u8; 32], y: [u8; 32], hash: [u8; 32] },
+	}
 
 	/// The Starknet pallet custom errors.
 	/// ERRORS
@@ -80,11 +82,15 @@ pub mod pallet {
 	/// The Starknet pallet internal functions.
 	impl<T: Config> Pallet<T> {
 		fn health_check() -> Result<(), DispatchError> {
-			// Compute a random hash.
-			/*let input_1 = FieldElement::from(1_u32);
-			let input_2 = FieldElement::from(2_u32);
-			let hash = hash::hash(hash::HashType::Pedersen, &input_1, &input_2);
-			log::info!("Random hash: {:?}", hash);*/
+			// Compute a hash of known values to check if the pallet is alive.
+			let x = FieldElement::from(1_u32);
+			let y = FieldElement::from(2_u32);
+			let hash = hash::hash(hash::HashType::Pedersen, &x, &y);
+			let x = x.to_bytes_be();
+			let y = y.to_bytes_be();
+			let hash = hash.to_bytes_be();
+			// Emit an event to notify the user.
+			Self::deposit_event(Event::HealthCheckHashComputed { x, y, hash });
 			Ok(())
 		}
 	}
