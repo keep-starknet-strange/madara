22bfbc19963cd7e8b5b7b0c6bbd77d5392537ccf
Evolve <Artevolve@yahoo.com>
2023-02-23 11:33:16 +0100

    feat: basic benchmarking 🚀

---
+++ benchmarking/scripts/metrics.js
@@ -0,0 +1,28 @@
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+
+async function main() {
+  const wsProvider = new WsProvider("ws://localhost:9944");
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const blockHash = await api.rpc.chain.getBlock();
+  const blockNumber = blockHash.block.header.number;
+
+  let totalExtrinsics = 0;
+
+  for (let i = blockNumber.toNumber() - 9; i <= blockNumber.toNumber(); i++) {
+    const hash = await api.rpc.chain.getBlockHash(i);
+    const block = await api.rpc.chain.getBlock(hash);
+    totalExtrinsics += block.block.extrinsics.length;
+  }
+
+  const avgExtrinsicsPerBlock = totalExtrinsics / 10;
+
+  console.log(
+    `Average extrinsics per block in the last 10 blocks: ${avgExtrinsicsPerBlock}`
+  );
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(-1);
+}).then(() => process.exit(0));
