2027f077cbe12769a550f2f5d6fd735663685c66
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-03-06 17:16:09 +0100

    💚 fix unit test

--- pallets/cairo/src/tests.rs
+++ pallets/cairo/src/tests.rs
@@ -9,7 +9,7 @@ fn given_normal_conditions_when_deploy_sierra_program_then_it_works() {
 		let deployer_origin = RuntimeOrigin::signed(deployer_account);
 		// Go past genesis block so events get deposited
 		System::set_block_number(1);
-		let sierra_code = BoundedVec::truncate_from(vec![1, 2, 3]);
+		let sierra_code = BoundedVec::truncate_from(vec![0; 32]);
 		// Dispatch a signed extrinsic.
 		assert_ok!(Cairo::deploy_sierra_program(deployer_origin, sierra_code.clone()));
 
