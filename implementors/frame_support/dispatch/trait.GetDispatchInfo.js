f43def1f45fe4ee3e4e1f43d55e21cf36bf06886
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-03-07 13:06:31 +0000

    deploy: c016378b62349813fe22ea50e312d2d39b435156

--- implementors/frame_support/dispatch/trait.GetDispatchInfo.js
+++ implementors/frame_support/dispatch/trait.GetDispatchInfo.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {
 "kaioshin_runtime":[["impl GetDispatchInfo for <a class=\"enum\" href=\"kaioshin_runtime/enum.RuntimeCall.html\" title=\"enum kaioshin_runtime::RuntimeCall\">RuntimeCall</a>"]],
 "pallet_cairo":[["impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_cairo/pallet/trait.Config.html\" title=\"trait pallet_cairo::pallet::Config\">Config</a>&gt; GetDispatchInfo for <a class=\"enum\" href=\"pallet_cairo/pallet/enum.Call.html\" title=\"enum pallet_cairo::pallet::Call\">Call</a>&lt;T&gt;"]],
+"pallet_kaioshin_randomness":[["impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_kaioshin_randomness/pallet/trait.Config.html\" title=\"trait pallet_kaioshin_randomness::pallet::Config\">Config</a>&gt; GetDispatchInfo for <a class=\"enum\" href=\"pallet_kaioshin_randomness/pallet/enum.Call.html\" title=\"enum pallet_kaioshin_randomness::pallet::Call\">Call</a>&lt;T&gt;"]],
 "pallet_starknet":[["impl&lt;T:&nbsp;<a class=\"trait\" href=\"pallet_starknet/pallet/trait.Config.html\" title=\"trait pallet_starknet::pallet::Config\">Config</a>&gt; GetDispatchInfo for <a class=\"enum\" href=\"pallet_starknet/pallet/enum.Call.html\" title=\"enum pallet_starknet::pallet::Call\">Call</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/pallet_kaioshin_randomness/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kaioshin_runtime":[["impl <a class=\"trait\" href=\"pallet_kaioshin_randomness/pallet/trait.Config.html\" title=\"trait pallet_kaioshin_randomness::pallet::Config\">Config</a> for <a class=\"struct\" href=\"kaioshin_runtime/struct.Runtime.html\" title=\"struct kaioshin_runtime::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
