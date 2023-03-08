969c8c5ae10a8fc068ae09530d8c557c6dc14ff7
LucasLvy <lucaslevy10@gmail.com>
2023-03-08 14:55:33 +0100

    chore(starknet): add the header fields

--- primitives/starknet/src/block/wrapper/header.rs
+++ primitives/starknet/src/block/wrapper/header.rs
@@ -16,17 +16,59 @@ use sp_core::{H256, U256};
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
 /// Starknet header definition.
 pub struct Header {
-	/// The block number.
+	/// The hash of this block’s parent.
+	pub parent_block_hash: U256,
+	/// The number (height) of this block.
 	pub block_number: U256,
-	/// The address of the sequencer.
+	/// The state commitment after this block.
+	pub global_state_root: U256,
+	/// The Starknet address of the sequencer who created this block.
 	pub sequencer_address: U256,
+	/// The time the sequencer created this block before executing transactions
+	pub block_timestamp: u128,
+	/// The number of transactions in a block
+	pub transaction_count: u128,
+	/// A commitment to the transactions included in the block
+	pub transaction_commitment: U256,
+	/// The number of events
+	pub event_count: u128,
+	/// A commitment to the events produced in this block
+	pub event_commitment: U256,
+	/// The version of the Starknet protocol used when creating this block
+	pub protocol_version: Option<u8>,
+	/// Extraneous data that might be useful for running transactions
+	pub extra_data: Option<U256>,
 }
 
 impl Header {
 	/// Creates a new header.
 	#[must_use]
-	pub fn new(block_number: U256, sequencer_address: U256) -> Self {
-		Self { block_number, sequencer_address }
+	pub fn new(
+		parent_block_hash: U256,
+		block_number: U256,
+		global_state_root: U256,
+		sequencer_address: U256,
+		block_timestamp: u128,
+		transaction_count: u128,
+		transaction_commitment: U256,
+		event_count: u128,
+		event_commitment: U256,
+		protocol_version: Option<u8>,
+		extra_data: Option<U256>,
+	) -> Self {
+		Self {
+			parent_block_hash,
+			block_number,
+			global_state_root,
+			sequencer_address,
+			block_timestamp,
+			transaction_count,
+			transaction_commitment,
+			event_count,
+			event_commitment,
+			protocol_version,
+			extra_data,
+		}
 	}
 
 	/// Compute the hash of the header.
