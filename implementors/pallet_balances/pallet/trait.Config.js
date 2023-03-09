1903b91bec115dcc374ac4ad556927567b09d11d
LucasLvy <LucasLvy@users.noreply.github.com>
2023-03-09 15:17:34 +0000

    deploy: dacf127d3702e6dc47b8bc617b3957b5c4d0e808

--- implementors/pallet_balances/pallet/trait.Config.js
+++ implementors/pallet_balances/pallet/trait.Config.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"kaioshin_runtime":[["impl Config&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"kaioshin_runtime/struct.Runtime.html\" title=\"struct kaioshin_runtime::Runtime\">Runtime</a>"]]
+"kaioshin_runtime":[["impl Config&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.68.0/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"kaioshin_runtime/struct.Runtime.html\" title=\"struct kaioshin_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
