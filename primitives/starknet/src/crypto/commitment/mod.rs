db48bfe5dfeee1142e1d77ad72f319c12bd81157
LucasLvy <lucaslevy10@gmail.com>
2023-03-15 12:51:44 +0100

    refacto(commitment): switch to compute hash on elements

--- primitives/starknet/src/crypto/commitment/mod.rs
+++ primitives/starknet/src/crypto/commitment/mod.rs
@@ -1,3 +1,5 @@
+use alloc::vec::Vec;
+
 use bitvec::vec::BitVec;
 use sp_core::H256;
 use starknet_crypto::FieldElement;
@@ -15,17 +17,17 @@ use crate::transaction::Transaction;
 /// of the index of the transaction / event within the block.
 ///
 /// The tree height is 64 in our case since our set operation takes u64 index values.
-struct CommitmentTree {
-    tree: MerkleTree<PedersenHasher>,
+struct CommitmentTree<T: CryptoHasher> {
+    tree: MerkleTree<T>,
 }
 
-impl Default for CommitmentTree {
+impl<T: CryptoHasher> Default for CommitmentTree<T> {
     fn default() -> Self {
         Self { tree: MerkleTree::empty() }
     }
 }
 
-impl CommitmentTree {
+impl<T: CryptoHasher> CommitmentTree<T> {
     pub fn set(&mut self, index: u64, value: FieldElement) {
         let key = index.to_be_bytes();
         self.tree.set(&BitVec::from(key.to_vec()), value)
@@ -33,6 +35,49 @@ impl CommitmentTree {
 
     pub fn commit(self) -> FieldElement {
         self.tree.commit()
+    }
+    /// Compute the combined hash of the transaction hash and the signature.
+    ///
+    /// Since the transaction hash doesn't take the signature values as its input
+    /// computing the transaction commitent uses a hash value that combines
+    /// the transaction hash with the array of signature values.
+    ///
+    /// # Arguments
+    ///
+    /// * `tx` - The transaction to compute the hash of.
+    ///
+    /// # Returns
+    ///
+    /// The transaction hash with signature.
+    pub fn calculate_transaction_hash_with_signature(&self, tx: &Transaction) -> FieldElement {
+        let signature_hash = self.compute_hash_on_elements(
+            &tx.signature
+                .iter()
+                .map(|elt| FieldElement::from_byte_slice_be(elt.as_bytes()).unwrap())
+                .collect::<Vec<FieldElement>>(),
+        );
+        pedersen::PedersenHasher::hash(FieldElement::from_byte_slice_be(tx.hash.as_bytes()).unwrap(), signature_hash)
+    }
+
+    /// Compute hash on elements, base on the [python implementation](https://github.com/starkware-libs/cairo-lang/blob/12ca9e91bbdc8a423c63280949c7e34382792067/src/starkware/cairo/common/hash_state.py#L6-L15).
+    ///
+    /// # Arguments
+    ///
+    /// * `elements` - The elements to hash.
+    ///
+    /// # Returns
+    ///
+    /// h(h(h(h(0, data[0]), data[1]), ...), data[n-1]), n).
+    pub fn compute_hash_on_elements(&self, elements: &[FieldElement]) -> FieldElement {
+        if elements.is_empty() {
+            <T as CryptoHasher>::hash(FieldElement::ZERO, FieldElement::ZERO)
+        } else {
+            let hash = elements.iter().fold(FieldElement::ZERO, |a, b| pedersen::PedersenHasher::hash(a, *b));
+            pedersen::PedersenHasher::hash(
+                hash,
+                FieldElement::from_byte_slice_be(&elements.len().to_be_bytes()).unwrap(),
+            )
+        }
     }
 }
 
@@ -42,66 +87,12 @@ impl CommitmentTree {
 /// constructed by adding the (transaction_index, transaction_hash_with_signature)
 /// key-value pairs to the tree and computing the root hash.
 pub fn calculate_transaction_commitment(transactions: &[Transaction]) -> H256 {
-    let mut tree = CommitmentTree::default();
+    let mut tree = CommitmentTree::<PedersenHasher>::default();
 
     transactions.iter().enumerate().for_each(|(idx, tx)| {
         let idx: u64 = idx.try_into().expect("too many transactions while calculating commitment");
-        let final_hash = calculate_transaction_hash_with_signature(tx);
+        let final_hash = tree.calculate_transaction_hash_with_signature(tx);
         tree.set(idx, final_hash);
     });
     H256::from_slice(&tree.commit().to_bytes_be())
-}
-
-/// Compute the combined hash of the transaction hash and the signature.
-///
-/// Since the transaction hash doesn't take the signature values as its input
-/// computing the transaction commitent uses a hash value that combines
-/// the transaction hash with the array of signature values.
-///
-/// Note that for non-invoke transactions we don't actually have signatures. The
-/// cairo-lang uses an empty list (whose hash is not the ZERO value!) in that
-/// case.
-fn calculate_transaction_hash_with_signature(tx: &Transaction) -> FieldElement {
-    lazy_static::lazy_static!(
-        static ref HASH_OF_EMPTY_LIST: FieldElement = HashChain::default().finalize();
-    );
-
-    let signature_hash = if tx.signature.is_empty() {
-        *HASH_OF_EMPTY_LIST
-    } else {
-        let mut hash = HashChain::default();
-        for signature in &tx.signature {
-            hash.update(FieldElement::from_byte_slice_be(signature.as_bytes()).unwrap());
-        }
-        hash.finalize()
-    };
-
-    pedersen::PedersenHasher::hash(FieldElement::from_byte_slice_be(tx.hash.as_bytes()).unwrap(), signature_hash)
-}
-
-/// HashChain is the structure used over at cairo side to represent the hash construction needed
-/// for computing the class hash.
-///
-/// Empty hash chained value equals `H(0, 0)` where `H` is the [`stark_hash()`] function, and the
-/// second value is the number of values hashed together in this chain. For other values, the
-/// accumulator is on each update replaced with the `H(hash, value)` and the number of count
-/// incremented by one.
-#[derive(Default)]
-pub struct HashChain {
-    hash: FieldElement,
-    count: usize,
-}
-
-impl HashChain {
-    /// Copy pasted from pathfinder ;).
-    pub fn update(&mut self, value: FieldElement) {
-        self.hash = pedersen::PedersenHasher::hash(self.hash, value);
-        self.count = self.count.checked_add(1).expect("could not have deserialized larger than usize Vecs");
-    }
-    /// Copy pasted from pathfinder ;).
-    pub fn finalize(self) -> FieldElement {
-        let count =
-            FieldElement::from_byte_slice_be(&self.count.to_be_bytes()).expect("usize is smaller than 251-bits");
-        pedersen::PedersenHasher::hash(self.hash, count)
-    }
 }
