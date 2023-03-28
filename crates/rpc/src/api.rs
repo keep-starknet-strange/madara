bb19a100a0a6c8e3cc34da5218518433010e1d42
drspacemn <dontpanicdao@gmail.com>
2023-03-27 19:40:17 -0600

    naming convention

--- crates/rpc/src/api.rs
+++ crates/rpc/src/api.rs
@@ -3,7 +3,7 @@ use jsonrpsee::{core::Error, proc_macros::rpc};
 use mp_starknet::starknet_block::block::Block;
 use sp_core::H256;
 
-#[rpc(server, namespace = "starknet")]
+#[rpc(server, client, namespace = "starknet")]
 pub trait StarknetRpcApi {
     #[method(name = "blockNumber")]
     fn block_hash(&self) -> Result<Option<H256>, Error>;
