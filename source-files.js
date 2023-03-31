769f665efd5c20214a2f2b58074b47a361b01bf9
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-03-31 10:39:19 +0000

    deploy: 86eaab52514233f1ba7757d365ea014f46e87bea

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "madara":["",[],["chain_spec.rs","lib.rs","rpc.rs","service.rs"]],\
-"madara_runtime":["",[],["lib.rs"]],\
+"madara_runtime":["",[],["config.rs","lib.rs","opaque.rs","pallets.rs","runtime_tests.rs","types.rs"]],\
 "mp_starknet":["",[["crypto",[["commitment",[],["mod.rs"]],["hash",[],["mod.rs","pedersen.rs","poseidon.rs"]],["merkle_patricia_tree",[],["merkle_node.rs","merkle_tree.rs","mod.rs"]]],["mod.rs"]],["execution",[],["mod.rs"]],["starknet_block",[],["block.rs","header.rs","mod.rs","serialize.rs"]],["starknet_serde",[],["mod.rs"]],["state",[],["mod.rs"]],["storage",[],["mod.rs"]],["traits",[],["hash.rs","mod.rs","transaction.rs"]],["transaction",[],["mod.rs","types.rs"]]],["lib.rs"]],\
 "pallet_starknet":["",[],["lib.rs","message.rs","state_root.rs","transaction_validation.rs","types.rs"]]\
 }');
