0f7ac0998dfbff61410e7ff2c18cee42fccba87c
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2023-04-02 12:51:07 +0200

    refacto: better API for storage overrides + doc

--- crates/primitives/starknet/src/storage/mod.rs
+++ crates/primitives/starknet/src/storage/mod.rs
@@ -4,8 +4,6 @@ use codec::{Decode, Encode};
 
 /// Current version of pallet Starknet's storage schema is stored under this key.
 pub const PALLET_STARKNET_SCHEMA: &[u8] = b":starknet_schema";
-/// Cached version of pallet Starknet's storage schema is stored under this key in the AuxStore.
-pub const PALLET_STARKNET_SCHEMA_CACHE: &[u8] = b":starknet_schema_cache";
 
 /// Pallet Starknet storage items.
 /// Pallet name.
@@ -16,14 +14,14 @@ pub const STARKNET_CURRENT_BLOCK: &[u8] = b"CurrentBlock";
 /// The schema version for Pallet Starknet's storage.
 #[derive(Clone, Copy, Debug, Encode, Decode, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-pub enum StarknetStorageSchema {
+pub enum StarknetStorageSchemaVersion {
     /// Undefined schema.
     Undefined,
     /// Schema V1.
     V1,
 }
 
-impl Default for StarknetStorageSchema {
+impl Default for StarknetStorageSchemaVersion {
     fn default() -> Self {
         Self::Undefined
     }
