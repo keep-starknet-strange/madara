6f2afc7e6ca22f0c89d931647a16abc82503aa40
Lucas @ StarkWare <70894690+LucasLvy@users.noreply.github.com>
2023-04-03 20:53:19 +0200

    Revert "refacto: better API for storage overrides + doc"
    This reverts commit 0f7ac0998dfbff61410e7ff2c18cee42fccba87c.

--- crates/client/storage/src/overrides/mod.rs
+++ crates/client/storage/src/overrides/mod.rs
@@ -3,9 +3,9 @@ use std::marker::PhantomData;
 use std::sync::Arc;
 
 use mp_starknet::block::StarknetBlock;
-use mp_starknet::storage::StarknetStorageSchemaVersion;
+use mp_starknet::storage::StarknetStorageSchema;
 use pallet_starknet::runtime_api::StarknetRuntimeApi;
-use sc_client_api::{Backend, HeaderBackend, StorageProvider};
+// Substrate
 use sp_api::ProvideRuntimeApi;
 use sp_io::hashing::twox_128;
 use sp_runtime::traits::Block as BlockT;
@@ -13,40 +13,16 @@ use sp_runtime::traits::Block as BlockT;
 mod schema_v1_override;
 
 pub use self::schema_v1_override::SchemaV1Override;
-use crate::onchain_storage_schema;
 
-/// A handle containing multiple enities implementing `StorageOverride`
-pub struct OverrideHandle<B: BlockT> {
-    /// Contains one implem of `StorageOverride` by version of the pallet storage schema
-    pub schemas: BTreeMap<StarknetStorageSchemaVersion, Box<dyn StorageOverride<B>>>,
-    /// A nonfailing way to retrieve the storage data
-    pub fallback: Box<dyn StorageOverride<B>>,
-}
-
-impl<B: BlockT> OverrideHandle<B> {
-    pub fn for_schema_version(&self, schema_version: &StarknetStorageSchemaVersion) -> &Box<dyn StorageOverride<B>> {
-        match self.schemas.get(schema_version) {
-            Some(storage_override) => storage_override,
-            None => &self.fallback,
-        }
-    }
-}
-
-impl<B: BlockT> OverrideHandle<B> {
-    pub fn for_block_hash<C: HeaderBackend<B> + StorageProvider<B, BE>, BE: Backend<B>>(
-        &self,
-        client: &C,
-        block_hash: B::Hash,
-    ) -> &Box<dyn StorageOverride<B>> {
-        let schema_version = onchain_storage_schema(client, block_hash);
-        self.for_schema_version(&schema_version)
-    }
+pub struct OverrideHandle<Block: BlockT> {
+    pub schemas: BTreeMap<StarknetStorageSchema, Box<dyn StorageOverride<Block>>>,
+    pub fallback: Box<dyn StorageOverride<Block>>,
 }
 
 /// Something that can fetch Starknet-related data. This trait is quite similar to the runtime API,
 /// and indeed the implementation of it uses the runtime API.
 /// Having this trait is useful because it allows optimized implementations that fetch data from a
-/// State Backend with some assumptions about pallet-staknet's storage schema. Using such an
+/// State Backend with some assumptions about pallet-ethereum's storage schema. Using such an
 /// optimized implementation avoids spawning a runtime and the overhead associated with it.
 pub trait StorageOverride<B: BlockT>: Send + Sync {
     /// Return the current block.
@@ -57,10 +33,8 @@ fn storage_prefix_build(module: &[u8], storage: &[u8]) -> Vec<u8> {
     [twox_128(module), twox_128(storage)].concat().to_vec()
 }
 
-/// A wrapper type for the Runtime API.
-///
-/// This type implements `StorageOverride`, so it can be used when calling the runtime API is
-/// desired but a `dyn StorageOverride` is required.
+/// A wrapper type for the Runtime API. This type implements `StorageOverride`, so it can be used
+/// when calling the runtime API is desired but a `dyn StorageOverride` is required.
 pub struct RuntimeApiStorageOverride<B: BlockT, C> {
     client: Arc<C>,
     _marker: PhantomData<B>,
@@ -78,6 +52,7 @@ where
     C: ProvideRuntimeApi<B> + Send + Sync,
     C::Api: StarknetRuntimeApi<B>,
 {
+    /// Return the current block.
     fn current_block(&self, block_hash: B::Hash) -> Option<StarknetBlock> {
         let api = self.client.runtime_api();
 
--- crates/primitives/starknet/src/storage/mod.rs
+++ crates/primitives/starknet/src/storage/mod.rs
@@ -4,6 +4,8 @@ use codec::{Decode, Encode};
 
 /// Current version of pallet Starknet's storage schema is stored under this key.
 pub const PALLET_STARKNET_SCHEMA: &[u8] = b":starknet_schema";
+/// Cached version of pallet Starknet's storage schema is stored under this key in the AuxStore.
+pub const PALLET_STARKNET_SCHEMA_CACHE: &[u8] = b":starknet_schema_cache";
 
 /// Pallet Starknet storage items.
 /// Pallet name.
@@ -14,14 +16,14 @@ pub const STARKNET_CURRENT_BLOCK: &[u8] = b"CurrentBlock";
 /// The schema version for Pallet Starknet's storage.
 #[derive(Clone, Copy, Debug, Encode, Decode, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-pub enum StarknetStorageSchemaVersion {
+pub enum StarknetStorageSchema {
     /// Undefined schema.
     Undefined,
     /// Schema V1.
     V1,
 }
 
-impl Default for StarknetStorageSchemaVersion {
+impl Default for StarknetStorageSchema {
     fn default() -> Self {
         Self::Undefined
     }
