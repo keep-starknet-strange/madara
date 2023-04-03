d8618249d357cc8bf0ecf902a19426cb1351d276
Lucas @ StarkWare <70894690+LucasLvy@users.noreply.github.com>
2023-04-03 20:53:19 +0200

    Revert "feat: create client/rpc and client/rpc-core creates"
    This reverts commit 4cba62ed82db1fa531a52508c1f7ff236d5d9a1b.

--- crates/pallets/starknet/src/runtime_api.rs
+++
@@ -1,8 +0,0 @@
-use sp_core::{H256, U256};
-
-sp_api::decl_runtime_apis! {
-    pub trait StarknetRuntimeApi {
-        fn current_block_number() -> U256;
-        fn current_block_hash() -> H256;
-    }
-}
