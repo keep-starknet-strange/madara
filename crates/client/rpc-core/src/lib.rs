de351c0db8ba52f36540651ff37cd27f8906f745
Lucas @ StarkWare <70894690+LucasLvy@users.noreply.github.com>
2023-04-03 20:53:19 +0200

    Revert "doc(client/rpc): add doc for crates rpc and rpc-core"
    This reverts commit ed60a8d6e66e48fbb45307fb9e1087b26463250c.

--- crates/client/rpc-core/src/lib.rs
+++ crates/client/rpc-core/src/lib.rs
@@ -1,8 +1,3 @@
-//! Starknet RPC API trait and types
-//!
-//! Starkware maintains (a description of Starknet API)[https://github.com/starkware-libs/starknet-specs/blob/master/api/starknet_api_openrpc.json] using the openRPC specification.
-//! This crate uses `jsonrpsee` to define such an API in Rust terms.
-
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::proc_macros::rpc;
 use serde::{Deserialize, Serialize};
@@ -11,7 +6,6 @@ pub type FieldElement = String;
 pub type BlockNumber = u64;
 pub type BlockHash = FieldElement;
 
-/// A tag specifying a dynamic reference to a blocl
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum BlockTag {
     #[serde(rename = "latest")]
@@ -26,7 +20,6 @@ pub struct BlockHashAndNumber {
     pub block_number: BlockNumber,
 }
 
-/// A block hash, number or tag
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(untagged)]
 pub enum BlockId {
@@ -35,18 +28,15 @@ pub enum BlockId {
     BlockTag(BlockTag),
 }
 
-/// Starknet rpc interface.
+/// Starkware rpc interface.
 #[rpc(server, namespace = "starknet")]
 pub trait StarknetRpcApi {
-    /// Get the most recent accepted block number
     #[method(name = "blockNumber")]
     fn block_number(&self) -> RpcResult<BlockNumber>;
 
-    /// Get the most recent accepted block hash and number
     #[method(name = "blockHashAndNumber")]
     fn block_hash_and_number(&self) -> RpcResult<BlockHashAndNumber>;
 
-    /// Get the number of transactions in a block given a block id
     #[method(name = "getBlockTransactionCount")]
     fn get_block_transaction_count(&self, block_id: BlockId) -> RpcResult<u128>;
 }
