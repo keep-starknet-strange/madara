fb5e7b524d3ce52ce7c4671614703c22eb3e942d
Lucas @ StarkWare <70894690+LucasLvy@users.noreply.github.com>
2023-04-03 20:53:19 +0200

    Revert "feat: use madara backend in the node services"
    This reverts commit 12277f9d81805e4ca2a0d9ac2a6ce955374a5661.

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -1,14 +1,13 @@
 //! Substrate Node Template CLI library.
 #![warn(missing_docs)]
 
+mod chain_spec;
 #[macro_use]
 mod service;
 mod benchmarking;
-mod chain_spec;
 mod cli;
 mod command;
 mod rpc;
-mod starknet;
 
 fn main() -> sc_cli::Result<()> {
     command::run()
