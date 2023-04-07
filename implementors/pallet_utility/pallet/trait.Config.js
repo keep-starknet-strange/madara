98a1b05ba78390318030f4d653490c4a0560397f
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-04-07 10:11:58 +0000

    deploy: 8852f7255b822156f8f479b8495a4ceda43bc858

---
+++ implementors/pallet_utility/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"madara_runtime":[["impl Config for <a class=\"struct\" href=\"madara_runtime/struct.Runtime.html\" title=\"struct madara_runtime::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
