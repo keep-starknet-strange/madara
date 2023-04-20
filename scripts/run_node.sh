b10779620dac4ae28c939abca0f3f27d89959b3e
Evolve <artevolve@yahoo.com>
2023-04-20 20:06:47 +0200

    fix: increase pool limits 🔨

--- scripts/run_node.sh
+++ scripts/run_node.sh
@@ -2,4 +2,4 @@
 # This script is meant to be run on Unix/Linux based systems
 set -e
 
-exec ./target/release/madara --dev --ws-external --execution native
+exec ./target/release/madara --dev --ws-external --execution native --pool-limit=100000 --pool-kbytes=500000
