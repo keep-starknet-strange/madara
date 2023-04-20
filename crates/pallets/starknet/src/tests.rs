ff44c7f488535a75313ec2026674540eafbdea82
chirag-bgh <chirag2bsingh@gmail.com>
2023-04-20 18:01:31 +0530

    wip

--- crates/pallets/starknet/src/tests.rs
+++ crates/pallets/starknet/src/tests.rs
@@ -493,73 +493,6 @@ fn given_non_root_when_set_fee_token_address_then_it_fails() {
 }
 
 #[test]
-fn test_verify_nonce() {
-    new_test_ext().execute_with(|| {
-        System::set_block_number(0);
-        run_to_block(2);
-
-        let contract_address_str = "02356b628D108863BAf8644c945d97bAD70190AF5957031f4852d00D0F690a77";
-        let contract_address_bytes = <[u8; 32]>::from_hex(contract_address_str).unwrap();
-        let class_hash_str = "025ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918";
-        let class_hash_bytes = <[u8; 32]>::from_hex(class_hash_str).unwrap();
-        // Example tx : https://testnet.starkscan.co/tx/0x6fc3466f58b5c6aaa6633d48702e1f2048fb96b7de25f2bde0bce64dca1d212
-        let tx = Transaction::new(
-            1_u8,
-            H256::from_str("0x06fc3466f58b5c6aaa6633d48702e1f2048fb96b7de25f2bde0bce64dca1d212").unwrap(),
-            bounded_vec![
-                H256::from_str("0x00f513fe663ffefb9ad30058bb2d2f7477022b149a0c02fb63072468d3406168").unwrap(),
-                H256::from_str("0x02e29e92544d31c03e89ecb2005941c88c28b4803a3647a7834afda12c77f096").unwrap()
-            ],
-            bounded_vec!(),
-            contract_address_bytes,
-            U256::from(0),
-            CallEntryPointWrapper::new(
-                Some(class_hash_bytes),
-                EntryPointTypeWrapper::External,
-                None,
-                bounded_vec![
-                    U256::from_str("0x024d1e355f6b9d27a5a420c8f4b50cea9154a8e34ad30fc39d7c98d3c177d0d7").unwrap(), // Contract address
-                    U256::from_str("0x00e7def693d16806ca2a2f398d8de5951344663ba77f340ed7a958da731872fc").unwrap(), // Selector
-                    U256::from_str("0x0000000000000000000000000000000000000000000000000000000000000001").unwrap(), // Length
-                    U256::from_str("0x0000000000000000000000000000000000000000000000000000000000000019").unwrap(), // Value
-                ],
-                contract_address_bytes,
-                contract_address_bytes,
-            ),
-            None,
-            None,
-        );
-        let state = &mut Starknet::create_state_reader().unwrap();
-        let invoke_tx = InvokeTransactionV1 {
-            transaction_hash: TransactionHash(StarkFelt::new(tx.hash.0).unwrap()),
-            max_fee: Fee(2),
-            signature: TransactionSignature(
-                tx.signature.clone().into_inner().iter().map(|x| StarkFelt::new(x.0).unwrap()).collect(),
-            ),
-            nonce: Nonce(StarkFelt::new(tx.nonce.into()).unwrap()),
-            sender_address: StarknetContractAddress::try_from(StarkFelt::new(tx.sender_address).unwrap()).unwrap(),
-            calldata: tx.call_entrypoint.to_starknet_call_entry_point().calldata,
-        };
-        let account_tx_context = AccountTransactionContext {
-            transaction_hash: invoke_tx.transaction_hash,
-            max_fee: invoke_tx.max_fee,
-            version: TransactionVersion(StarkFelt::from(1)),
-            signature: invoke_tx.signature.clone(),
-            nonce: invoke_tx.nonce,
-            sender_address: invoke_tx.sender_address,
-        };
-        // Test for a valid nonce
-        let result = tx.verify_nonce(&account_tx_context, state);
-        assert!(result.is_ok());
-
-        // Test for an invalid nonce
-        let account_tx_context_invalid_nonce = AccountTransactionContext { nonce: Nonce(2.into()), ..account_tx_context };
-        let result = tx.verify_nonce(&account_tx_context_invalid_nonce, state);
-        assert!(result.is_err());
-    });
-}
-
-#[test]
 fn given_erc20_transfer_when_invoke_then_it_works() {
     new_test_ext().execute_with(|| {
         System::set_block_number(0);
@@ -647,4 +580,44 @@ fn get_contract_class_wrapper(contract_content: &'static [u8]) -> ContractClassW
     let contract_class: ContractClass =
         serde_json::from_slice(contract_content).expect("File must contain the content of a compiled contract.");
     ContractClassWrapper::from(contract_class)
+}
+
+#[test]
+fn test_verify_nonce() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let json_content: &str = include_str!("../../../../resources/transactions/invoke.json");
+        let tx = transaction_from_json(json_content, &[]).expect("Failed to create Transaction from JSON");
+
+        let state = &mut Starknet::create_state_reader().unwrap();
+        let invoke_tx = InvokeTransactionV1 {
+            transaction_hash: TransactionHash(StarkFelt::new(tx.hash.0).unwrap()),
+            max_fee: Fee(2),
+            signature: TransactionSignature(
+                tx.signature.clone().into_inner().iter().map(|x| StarkFelt::new(x.0).unwrap()).collect(),
+            ),
+            nonce: Nonce(StarkFelt::new(tx.nonce.into()).unwrap()),
+            sender_address: StarknetContractAddress::try_from(StarkFelt::new(tx.sender_address).unwrap()).unwrap(),
+            calldata: tx.call_entrypoint.to_starknet_call_entry_point().calldata,
+        };
+        let account_tx_context = AccountTransactionContext {
+            transaction_hash: invoke_tx.transaction_hash,
+            max_fee: invoke_tx.max_fee,
+            version: TransactionVersion(StarkFelt::from(1)),
+            signature: invoke_tx.signature.clone(),
+            nonce: invoke_tx.nonce,
+            sender_address: invoke_tx.sender_address,
+        };
+        // Test for a valid nonce
+        let result = tx.verify_nonce(&account_tx_context, state);
+        assert!(result.is_ok());
+
+        // Test for an invalid nonce
+        let account_tx_context_invalid_nonce =
+            AccountTransactionContext { nonce: Nonce(2.into()), ..account_tx_context };
+        let result = tx.verify_nonce(&account_tx_context_invalid_nonce, state);
+        assert!(result.is_err());
+    });
 }
