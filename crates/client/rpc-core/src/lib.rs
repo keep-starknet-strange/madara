5eb7da90ad40de6e1df158b588eda5bf6faf6357
Timothée Delabrouille <34384633+tdelabro@users.noreply.github.com>
2023-04-20 19:26:55 +0200

    Push whole block to digest (#241)

--- crates/client/rpc-core/src/lib.rs
+++ crates/client/rpc-core/src/lib.rs
@@ -4,6 +4,9 @@
 //! using the openRPC specification.
 //! This crate uses `jsonrpsee` to define such an API in Rust terms.
 
+#[cfg(test)]
+mod tests;
+
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::proc_macros::rpc;
 use serde::{Deserialize, Serialize};
@@ -15,8 +18,10 @@ pub type BlockHash = FieldElement;
 /// A tag specifying a dynamic reference to a block
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum BlockTag {
+    /// The latest accepted block
     #[serde(rename = "latest")]
     Latest,
+    /// The current pending block
     #[serde(rename = "pending")]
     Pending,
 }
@@ -35,16 +40,59 @@ pub struct FunctionCall {
     pub calldata: Vec<FieldElement>,
 }
 
-/// A block hash, number or tag
-/// TODO: fix block_tag
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum BlockId {
-    #[serde(rename = "block_hash")]
-    BlockHash(BlockHash),
-    #[serde(rename = "block_number")]
-    BlockNumber(BlockNumber),
-    BlockTag(BlockTag),
+// In order to mix tagged and untagged {de}serialization for BlockId (see starknet RPC standard)
+// in the same object, we need this kind of workaround with intermediate types
+mod block_id {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Clone)]
+    enum BlockIdTaggedVariants {
+        #[serde(rename = "block_hash")]
+        BlockHash(BlockHash),
+        #[serde(rename = "block_number")]
+        BlockNumber(BlockNumber),
+    }
+
+    #[derive(Serialize, Deserialize, Clone)]
+    #[serde(untagged)]
+    enum BlockIdUntagged {
+        Tagged(BlockIdTaggedVariants),
+        BlockTag(BlockTag),
+    }
+
+    /// A block hash, number or tag
+    #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+    #[serde(from = "BlockIdUntagged")]
+    #[serde(into = "BlockIdUntagged")]
+    pub enum BlockId {
+        BlockHash(BlockHash),
+        BlockNumber(BlockNumber),
+        BlockTag(BlockTag),
+    }
+
+    impl From<BlockIdUntagged> for BlockId {
+        fn from(value: BlockIdUntagged) -> Self {
+            match value {
+                BlockIdUntagged::Tagged(v) => match v {
+                    BlockIdTaggedVariants::BlockHash(h) => Self::BlockHash(h),
+                    BlockIdTaggedVariants::BlockNumber(n) => Self::BlockNumber(n),
+                },
+                BlockIdUntagged::BlockTag(t) => Self::BlockTag(t),
+            }
+        }
+    }
+
+    impl From<BlockId> for BlockIdUntagged {
+        fn from(value: BlockId) -> Self {
+            match value {
+                BlockId::BlockHash(h) => Self::Tagged(BlockIdTaggedVariants::BlockHash(h)),
+                BlockId::BlockNumber(n) => Self::Tagged(BlockIdTaggedVariants::BlockNumber(n)),
+                BlockId::BlockTag(t) => Self::BlockTag(t),
+            }
+        }
+    }
 }
+pub use block_id::BlockId;
 
 /// Starknet rpc interface.
 #[rpc(server, namespace = "starknet")]
