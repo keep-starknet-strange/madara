7fc33e5c1361ef23fa8fddac9fc587ec5e6d35fc
LucasLvy <lucaslevy10@gmail.com>
2023-05-03 10:55:16 +0200

    test(deploy account): fix another test

--- crates/pallets/starknet/src/tests/deploy_account_tx.rs
+++ crates/pallets/starknet/src/tests/deploy_account_tx.rs
@@ -3,17 +3,39 @@ use core::str::FromStr;
 use frame_support::{assert_err, assert_ok, bounded_vec, BoundedVec};
 use hex::FromHex;
 use mp_starknet::execution::types::{CallEntryPointWrapper, EntryPointTypeWrapper};
-use mp_starknet::transaction::types::Transaction;
+use mp_starknet::transaction::types::{EventWrapper, Transaction};
 use sp_core::{H256, U256};
 
 use super::mock::*;
-use crate::Error;
+use crate::{Error, Event, StorageView};
 
 #[test]
 fn given_contract_run_deploy_account_tx_works() {
     new_test_ext().execute_with(|| {
         System::set_block_number(0);
         run_to_block(2);
+        // pedersen(sn_keccak(b"ERC20_balances"),
+        // 0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888) which is the key in the
+        // starknet contract for
+        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).low
+        StorageView::<Test>::insert(
+            (
+                Starknet::fee_token_address(),
+                H256::from_str("0x0202fb79bbf78d825c665d496594cbc6e9ddc4bb0a0e8506f361ba208c647766").unwrap(),
+            ),
+            U256::from(u128::MAX),
+        );
+        // pedersen(sn_keccak(b"ERC20_balances"),
+        // 0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888) + 1 which is the key in the
+        // starknet contract for
+        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).high
+        StorageView::<Test>::insert(
+            (
+                Starknet::fee_token_address(),
+                H256::from_str("0x0202fb79bbf78d825c665d496594cbc6e9ddc4bb0a0e8506f361ba208c647767").unwrap(),
+            ),
+            U256::from(u128::MAX),
+        );
 
         let none_origin = RuntimeOrigin::none();
         // TEST ACCOUNT CONTRACT
@@ -37,6 +59,19 @@ fn given_contract_run_deploy_account_tx_works() {
 
         assert_ok!(Starknet::deploy_account(none_origin, transaction));
         assert_eq!(Starknet::contract_class_hash_by_address(test_addr).unwrap(), account_class_hash);
+        let expected_fee_transfer_event = Event::StarknetEvent(EventWrapper {
+                keys: bounded_vec![
+                    H256::from_str("0x0099cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9").unwrap()
+                ],
+                data: bounded_vec!(
+                    H256::from_slice(&test_addr), // From
+                    H256::from_str("0x0000000000000000000000000000000000000000000000000000000000000002").unwrap(), // To
+                    H256::from_str("0x000000000000000000000000000000000000000000000000000000000001be90").unwrap(), // Amount low
+                    H256::zero(), // Amount high
+                ),
+                from_address: Starknet::fee_token_address(),
+            }).into();
+        System::assert_last_event(expected_fee_transfer_event)
     });
 }
 
