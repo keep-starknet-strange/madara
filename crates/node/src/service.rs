751596e14fa1be4e0a910816dab9bb1e8f048eb2
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-05-04 10:33:04 +0200

    🚨 fix cargo clippy

--- crates/node/src/service.rs
+++ crates/node/src/service.rs
@@ -100,7 +100,7 @@ where
         })
         .transpose()?;
 
-    let executor = sc_service::new_native_or_wasm_executor(&config);
+    let executor = sc_service::new_native_or_wasm_executor(config);
 
     let (client, backend, keystore_container, task_manager) = sc_service::new_full_parts::<Block, RuntimeApi, _>(
         config,
