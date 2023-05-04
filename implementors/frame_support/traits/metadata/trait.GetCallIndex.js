8e0a3a07ec6a46f94342bd3082a99694530c6afb
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-05-04 09:49:06 +0000

    deploy: d14bee764a4b16804adb6efcc12321a42c9e61e7

---
+++ implementors/frame_support/traits/metadata/trait.GetCallIndex.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pallet_starknet":[["impl&lt;T: <a class=\"trait\" href=\"pallet_starknet/pallet/trait.Config.html\" title=\"trait pallet_starknet::pallet::Config\">Config</a>&gt; GetCallIndex for <a class=\"enum\" href=\"pallet_starknet/pallet/enum.Call.html\" title=\"enum pallet_starknet::pallet::Call\">Call</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/sc_executor/executor/trait.NativeExecutionDispatch.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"madara":[["impl NativeExecutionDispatch for <a class=\"struct\" href=\"madara/service/struct.ExecutorDispatch.html\" title=\"struct madara::service::ExecutorDispatch\">ExecutorDispatch</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
