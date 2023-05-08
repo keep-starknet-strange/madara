56bf9d35a405124668cbe952ce7a2e54e62c2553
Gregory Edison <gregory.edison1993@gmail.com>
2023-05-08 16:35:15 +0200

    validate deploy testing

--- crates/pallets/starknet/src/tests/deploy_account_tx.rs
+++ crates/pallets/starknet/src/tests/deploy_account_tx.rs
@@ -4,6 +4,8 @@ use frame_support::{assert_err, assert_ok, bounded_vec, BoundedVec};
 use hex::FromHex;
 use mp_starknet::transaction::types::{DeployAccountTransaction, EventWrapper};
 use sp_core::{H256, U256};
+use starknet_core::types::FieldElement;
+use starknet_core::utils::get_storage_var_address;
 
 use super::mock::*;
 use crate::{Error, Event, StorageView};
@@ -13,33 +15,13 @@ fn given_contract_run_deploy_account_tx_works() {
     new_test_ext().execute_with(|| {
         System::set_block_number(0);
         run_to_block(2);
-        // pedersen(sn_keccak(b"ERC20_balances"),
-        // 0x00a13c294af26c4e940d28b1db914e4bb28158638deeeb4ae9ca9b37ab3e4a97) which is the key in the
-        // starknet contract for
-        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).low
-        StorageView::<MockRuntime>::insert(
-            (
-                Starknet::fee_token_address(),
-                H256::from_str("0x02450cb55e7682ffca9e1db504e2de1263d587242b9b43e27a1eedf18b4bbabf").unwrap(),
-            ),
-            U256::from(u128::MAX),
-        );
-        // pedersen(sn_keccak(b"ERC20_balances"),
-        // 0x00a13c294af26c4e940d28b1db914e4bb28158638deeeb4ae9ca9b37ab3e4a97) + 1 which is the key in the
-        // starknet contract for
-        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).high
-        StorageView::<MockRuntime>::insert(
-            (
-                Starknet::fee_token_address(),
-                H256::from_str("0x02450cb55e7682ffca9e1db504e2de1263d587242b9b43e27a1eedf18b4bbabf").unwrap(),
-            ),
-            U256::from(u128::MAX),
-        );
         let none_origin = RuntimeOrigin::none();
         // TEST ACCOUNT CONTRACT
         // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
         let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
-        let (test_addr, account_class_hash, calldata) = no_validate_account_helper(salt);
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::NoValidate);
+
+        set_infinite_tokens(test_addr);
 
         let transaction = DeployAccountTransaction {
             account_class_hash,
@@ -76,31 +58,11 @@ fn given_contract_run_deploy_account_tx_twice_fails() {
     new_test_ext().execute_with(|| {
         System::set_block_number(0);
         run_to_block(2);
-        // pedersen(sn_keccak(b"ERC20_balances"),
-        // 0x00a13c294af26c4e940d28b1db914e4bb28158638deeeb4ae9ca9b37ab3e4a97) which is the key in the
-        // starknet contract for
-        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).low
-        StorageView::<MockRuntime>::insert(
-            (
-                Starknet::fee_token_address(),
-                H256::from_str("0x02450cb55e7682ffca9e1db504e2de1263d587242b9b43e27a1eedf18b4bbabf").unwrap(),
-            ),
-            U256::from(u128::MAX),
-        );
-        // pedersen(sn_keccak(b"ERC20_balances"),
-        // 0x00a13c294af26c4e940d28b1db914e4bb28158638deeeb4ae9ca9b37ab3e4a97) + 1 which is the key in the
-        // starknet contract for
-        // ERC20_balances(0x015c7ddf2e57acc45be4c0dfa3c7b1aca474eeaab0041b89a30317238401c888).high
-        StorageView::<MockRuntime>::insert(
-            (
-                Starknet::fee_token_address(),
-                H256::from_str("0x02450cb55e7682ffca9e1db504e2de1263d587242b9b43e27a1eedf18b4bbabf").unwrap(),
-            ),
-            U256::from(u128::MAX),
-        );
         let none_origin = RuntimeOrigin::none();
         let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
-        let (test_addr, account_class_hash, calldata) = no_validate_account_helper(salt);
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::NoValidate);
+
+        set_infinite_tokens(test_addr);
 
         // TEST ACCOUNT CONTRACT
         // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
@@ -133,7 +95,7 @@ fn given_contract_run_deploy_account_tx_undeclared_then_it_fails() {
         let none_origin = RuntimeOrigin::none();
         let rand_address =
             <[u8; 32]>::from_hex("0000000000000000000000000000000000000000000000000000000000001234").unwrap();
-        let (_, account_class_hash, _) = account_helper(salt);
+        let (_, account_class_hash, _) = account_helper_argent_v0(salt);
         let transaction = DeployAccountTransaction {
             account_class_hash,
             sender_address: rand_address,
@@ -162,7 +124,7 @@ fn given_contract_run_deploy_account_tx_fails_wrong_tx_version() {
         // TEST ACCOUNT CONTRACT
         // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
         let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
-        let (test_addr, account_class_hash, calldata) = account_helper(salt);
+        let (test_addr, account_class_hash, calldata) = account_helper_argent_v0(salt);
 
         let wrong_tx_version = 50_u8;
 
@@ -183,4 +145,234 @@ fn given_contract_run_deploy_account_tx_fails_wrong_tx_version() {
             Error::<MockRuntime>::TransactionExecutionFailed
         );
     });
+}
+
+#[test]
+fn given_contract_run_deploy_account_openzeppelin_tx_works() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::Openzeppelin);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Openzeppelin);
+        let tx_hash = H256::from_str("0x06ff0e0245daed20c0b4f21ae5c9286ba3a03e0c62b2bec2d0dcec2a4d6b9889").unwrap();
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: sign_message_hash(tx_hash),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_ok!(Starknet::deploy_account(none_origin, transaction));
+        assert_eq!(Starknet::contract_class_hash_by_address(test_addr).unwrap(), account_class_hash);
+    });
+}
+
+#[test]
+fn given_contract_run_deploy_account_openzeppelin_with_incorrect_signature_then_it_fails() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::Openzeppelin);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Openzeppelin);
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: bounded_vec!(H256::from_low_u64_be(1), H256::from_low_u64_be(1)),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_err!(Starknet::deploy_account(none_origin, transaction), Error::<Test>::TransactionExecutionFailed);
+    });
+}
+
+#[test]
+fn given_contract_run_deploy_account_argent_tx_works() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::Argent);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Argent);
+        let tx_hash = H256::from_str("0x0781152a4f3fc0dada10f24a40f7499ce3c17c3867acae82024f5507475f89da").unwrap();
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: sign_message_hash(tx_hash),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_ok!(Starknet::deploy_account(none_origin, transaction));
+        assert_eq!(Starknet::contract_class_hash_by_address(test_addr).unwrap(), account_class_hash);
+    });
+}
+
+#[test]
+fn given_contract_run_deploy_account_argent_with_incorrect_signature_then_it_fails() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, account_class_hash, calldata) = account_helper(salt, AccountType::Argent);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Argent);
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: bounded_vec!(H256::from_low_u64_be(1), H256::from_low_u64_be(1)),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_err!(Starknet::deploy_account(none_origin, transaction), Error::<Test>::TransactionExecutionFailed);
+    });
+}
+
+#[test]
+fn given_contract_run_deploy_account_braavos_tx_works() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, proxy_class_hash, mut calldata) = account_helper(salt, AccountType::BraavosProxy);
+        calldata.push("0x1");
+        calldata.push(ACCOUNT_PUBLIC_KEY);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Braavos);
+
+        let tx_hash = H256::from_str("0x06ae3d81978d498def89e1121b2d84a873d63c30d80f7ed81e2dc9be6a961770").unwrap();
+
+        let mut signatures: Vec<H256> = sign_message_hash(tx_hash).into();
+        let empty_signatures = [H256::from_low_u64_be(0); 8];
+        signatures.append(&mut empty_signatures.to_vec());
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash: proxy_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: signatures.try_into().unwrap(),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_ok!(Starknet::deploy_account(none_origin, transaction));
+        assert_eq!(Starknet::contract_class_hash_by_address(test_addr).unwrap(), proxy_class_hash);
+    });
+}
+
+#[test]
+fn given_contract_run_deploy_account_braavos_with_incorrect_signature_then_it_fails() {
+    new_test_ext().execute_with(|| {
+        System::set_block_number(0);
+        run_to_block(2);
+
+        let none_origin = RuntimeOrigin::none();
+        // TEST ACCOUNT CONTRACT
+        // - ref testnet tx(0x0751b4b5b95652ad71b1721845882c3852af17e2ed0c8d93554b5b292abb9810)
+        let salt = "0x03b37cbe4e9eac89d54c5f7cc6329a63a63e8c8db2bf936f981041e086752463";
+        let (test_addr, proxy_class_hash, mut calldata) = account_helper(salt, AccountType::BraavosProxy);
+        calldata.push("0x1");
+        calldata.push(ACCOUNT_PUBLIC_KEY);
+
+        set_infinite_tokens(test_addr);
+        set_signer(test_addr, AccountType::Braavos);
+
+        let transaction = DeployAccountTransaction {
+            account_class_hash: proxy_class_hash,
+            sender_address: test_addr,
+            salt: U256::from_str(salt).unwrap(),
+            version: 1,
+            calldata: BoundedVec::try_from(calldata.clone().into_iter().map(U256::from).collect::<Vec<U256>>())
+                .unwrap(),
+            nonce: U256::zero(),
+            signature: [H256::from_low_u64_be(0); 10].to_vec().try_into().unwrap(),
+            max_fee: U256::from(u128::MAX),
+        };
+
+        assert_err!(Starknet::deploy_account(none_origin, transaction), Error::<Test>::TransactionExecutionFailed);
+    });
+}
+
+fn set_infinite_tokens(address: [u8; 32]) {
+    let storage_key_low =
+        get_storage_var_address("ERC20_balances", &[FieldElement::from_bytes_be(&address).unwrap()]).unwrap();
+    let storage_key_high = storage_key_low + FieldElement::ONE;
+    StorageView::<Test>::insert(
+        (Starknet::fee_token_address(), H256::from(storage_key_low.to_bytes_be())),
+        U256::from(u128::MAX),
+    );
+    StorageView::<Test>::insert(
+        (Starknet::fee_token_address(), H256::from(storage_key_high.to_bytes_be())),
+        U256::from(u128::MAX),
+    );
+}
+
+fn set_signer(address: [u8; 32], account_type: AccountType) {
+    let (var_name, args) = match account_type {
+        AccountType::Argent => ("_signer", vec![]),
+        AccountType::Braavos => ("Account_signers", vec![FieldElement::ZERO]),
+        AccountType::Openzeppelin => ("Account_public_key", vec![]),
+        AccountType::BraavosProxy => return,
+        AccountType::NoValidate => return,
+    };
+    let storage_key = get_storage_var_address(var_name, &args).unwrap();
+    StorageView::<Test>::insert(
+        (address, H256::from(storage_key.to_bytes_be())),
+        U256::from_str(ACCOUNT_PUBLIC_KEY).unwrap(),
+    );
 }
