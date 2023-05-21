cd774052df07e5a56ac4c7df3d385ff89c68333b
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-05-21 13:55:34 +0000

    deploy: 62609f6808c882f8fe2301af2f4a14e56e06ebfb

--- pallet_starknet/types/sidebar-items.js
+++ pallet_starknet/types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["DeployAccountTransactionOutput"],"type":["ContractClassMapping","ContractStorageKeyWrapper","MaxTransactionsPendingBlock","NonceWrapper","StarkFeltWrapper","StorageKeyWrapper"]};
+window.SIDEBAR_ITEMS = {"struct":["DeployAccountTransactionOutput"],"type":["ContractClassMapping","ContractStorageKeyWrapper","MaxTransactionsPendingBlock","NonceWrapper","StorageKeyWrapper"]};
