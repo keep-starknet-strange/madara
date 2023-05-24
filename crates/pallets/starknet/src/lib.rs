67cc38d188737e46b7526483a7c17cdaa4efe582
LucasLvy <lucaslevy10@gmail.com>
2023-05-24 17:56:54 +0200

    add comments

--- crates/pallets/starknet/src/lib.rs
+++ crates/pallets/starknet/src/lib.rs
@@ -728,7 +728,10 @@ pub mod pallet {
         /// are being whitelisted and marked as valid.
         fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
             // TODO: Call `__validate__` entrypoint of the contract. #69
-
+            // The priority right now is the max u64 - nonce because for unsigned transactions we need to
+            // determine an absolute priority. For now we use that for the benchmark (lowest nonce goes first)
+            // otherwise we have a nonce error and everything fails.
+            // Once we have a real fee market this is where we'll chose the most profitable transaction.
             match call {
                 Call::invoke { transaction } => ValidTransaction::with_tag_prefix("starknet")
                     .priority(u64::MAX - transaction.nonce.as_u64())
