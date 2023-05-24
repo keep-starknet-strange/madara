ad0f54abfa475d273442aae3d942867e7e41960f
EvolveArt <EvolveArt@users.noreply.github.com>
2023-05-24 15:22:30 +0000

    deploy: 0e05a69d0b10e8df961e4eb7fb159cddac266e0a

--- mp_starknet/crypto/hash/sidebar-items.js
+++ mp_starknet/crypto/hash/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["HashType"],"fn":["hash","hash_field"],"mod":["pedersen","poseidon"]};
+window.SIDEBAR_ITEMS = {"enum":["HashType"],"fn":["hash"],"mod":["pedersen","poseidon"]};
--- mp_starknet/execution/sidebar-items.js
+++ mp_starknet/execution/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["deserialize_bounded_btreemap","deserialize_option_bounded_btreemap","serialize_bounded_btreemap","serialize_option_bounded_btreemap"],"mod":["call_entrypoint_wrapper","contract_class_wrapper","entrypoint_wrapper","program_wrapper","types"]};
+window.SIDEBAR_ITEMS = {"fn":["deserialize_bounded_btreemap","deserialize_option_bounded_btreemap","serialize_bounded_btreemap","serialize_option_bounded_btreemap"],"mod":["call_entrypoint_wrapper","contract_class_wrapper","entrypoint_wrapper","felt252_wrapper","program_wrapper","types"]};
