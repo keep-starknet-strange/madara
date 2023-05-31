b005a8bf630bc1fec9c9f35728294427d791dca4
github-action-benchmark <github@users.noreply.github.com>
2023-05-31 09:02:20 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for b65bc781493f0f38764980c95ccd213e993a323b

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1685523739631,
+  "repoUrl": "https://github.com/keep-starknet-strange/madara",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "5263301+d-roak@users.noreply.github.com",
+            "name": "Oak",
+            "username": "d-roak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b65bc781493f0f38764980c95ccd213e993a323b",
+          "message": "⬆️ upgrade substrate to polkadot-v0.9.43 (#497)\n\nCo-authored-by: Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>",
+          "timestamp": "2023-05-31T10:59:02+02:00",
+          "tree_id": "5466e92e56f1a884b2acfcff5a3e7aef5788aadb",
+          "url": "https://github.com/keep-starknet-strange/madara/commit/b65bc781493f0f38764980c95ccd213e993a323b"
+        },
+        "date": 1685523739138,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Average Extrinsics per block",
+            "value": 350,
+            "unit": "extrinsics/block"
+          },
+          {
+            "name": "Average TPS",
+            "value": 58.333333333333336,
+            "unit": "tps"
+          }
+        ]
+      }
+    ]
+  }
+}
