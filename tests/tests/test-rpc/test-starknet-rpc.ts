ab9553101e706de11a54897ce66a2a5597714308
chirag-bgh <chirag2bsingh@gmail.com>
2023-06-01 23:52:38 +0530

    fix declare tests

--- tests/tests/test-rpc/test-starknet-rpc.ts
+++ tests/tests/test-rpc/test-starknet-rpc.ts
@@ -620,7 +620,7 @@ describeDevMadara("Starknet RPC", (context) => {
       const resp = await account.declare(
         {
           classHash:
-            "0x057eca87f4b19852cfd4551cf4706ababc6251a8781733a0a11cf8e94211da95",
+            "0",
           contract,
         },
         { nonce, version: 1, maxFee: "123456" }
@@ -684,7 +684,7 @@ describeDevMadara("Starknet RPC", (context) => {
       await account.declare(
         {
           classHash:
-            "0x057eca87f4b19852cfd4551cf4706ababc6251a8781733a0a11cf8e94211da96",
+            "0",
           contract,
         },
         { nonce, version: 1, maxFee: "123456" }
