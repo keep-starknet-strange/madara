14dde6dc161d5f3e780d6306831993c0fca922d0
Ben Goebel <dontpanicdao@gmail.com>
2023-06-06 10:32:47 -0600

    update state - ethers

--- crates/primitives/starknet/src/data_availability/ethereum/mod.rs
+++ crates/primitives/starknet/src/data_availability/ethereum/mod.rs
@@ -1,4 +1,5 @@
-use ethers;
+use async_trait::async_trait;
+use ethers::prelude::*;
 
 use super::DataAvailability;
 /// Ethereum data availability configuration.
@@ -14,16 +15,40 @@ pub struct EthereumDataAvailability {
     pub config: EthereumDataAvailabilityConfig,
 }
 
+#[async_trait]
 impl DataAvailability for EthereumDataAvailability {
     /// Publish data to Ethereum.
     /// # Arguments
     /// * `sender_id` - The sender id.
     /// * `data` - The data to publish.
-    fn publish_data(&self, sender_id: &[u8], data: &[u8]) -> Result<(), &str> {
+    async fn publish_data(&self, sender_id: &[u8], data: &[u8]) -> Result<(), &str> {
         self.check_data(data)?;
         // Send data to Ethereum.
         // Check the result
         // Return the result.
+            /**
+         Updates the state of the StarkNet, based on a proof of the
+        StarkNet OS that the state transition is valid.
+
+        Arguments:
+            programOutput - The main part of the StarkNet OS program output.
+            data_availability_fact - An encoding of the on-chain data associated
+            with the 'programOutput'.
+        */
+        
+        abigen!(
+            STARKNET,
+            r#"[
+                function updateState(
+                    uint256[] calldata programOutput,
+                    uint256 onchainDataHash,
+                    uint256 onchainDataSize
+                ) external onlyOperator
+            ]"#,
+        );
+
+        // let provider = Provider::<Http>::try_from(RPC_URL)?;
+        // let client = Arc::new(provider);
         todo!()
     }
 }
--- crates/primitives/starknet/src/data_availability/mod.rs
+++ crates/primitives/starknet/src/data_availability/mod.rs
@@ -1,10 +1,13 @@
 //! Data availability module.
 pub mod ethereum;
 
+use async_trait::async_trait;
+
+#[async_trait]
 pub trait DataAvailability {
     /// Publish data to Ethereum.
     /// # Arguments
     /// * `sender_id` - The sender id.
     /// * `data` - The data to publish.
-    fn publish_data(&self, sender_id: &[u8], data: &[u8]) -> Result<(), &str>;
+    async fn publish_data(&self, sender_id: &[u8], data: &[u8]) -> Result<(), &str>;
 }
