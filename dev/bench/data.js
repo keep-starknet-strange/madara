3590f0195d829caf77bcd23b54008c42d273798f
github-action-benchmark <github@users.noreply.github.com>
2023-06-07 06:25:52 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 8254b325a22e30f43fc74df422f3c05a9c667e83

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686053483642,
+  "lastUpdate": 1686119152128,
   "repoUrl": "https://github.com/keep-starknet-strange/madara",
   "entries": {
     "Benchmark": [
@@ -1712,6 +1712,42 @@ window.BENCHMARK_DATA = {
             "value": 203.66666666666666,
             "unit": "tps",
             "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3612\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "5263301+d-roak@users.noreply.github.com",
+            "name": "Oak",
+            "username": "d-roak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8254b325a22e30f43fc74df422f3c05a9c667e83",
+          "message": "fix: Refactor docker for multi arch (#549)",
+          "timestamp": "2023-06-07T08:22:41+02:00",
+          "tree_id": "1ae1d46f28c4051a7388bb1ce241afa4766f236a",
+          "url": "https://github.com/keep-starknet-strange/madara/commit/8254b325a22e30f43fc74df422f3c05a9c667e83"
+        },
+        "date": 1686119151614,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Average Extrinsics per block",
+            "value": 1227.25,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3599\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
+          },
+          {
+            "name": "Average TPS",
+            "value": 204.54166666666666,
+            "unit": "tps",
+            "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3599\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
           }
         ]
       }
