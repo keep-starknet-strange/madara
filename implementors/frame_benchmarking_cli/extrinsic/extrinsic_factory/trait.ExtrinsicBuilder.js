cc1746a26c9a5a850f93971068ec20a82d744ce0
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-06-09 10:57:51 +0000

    deploy: 6a081c663542eeefa51c02c843d36fc5670dda8c

--- implementors/frame_benchmarking_cli/extrinsic/extrinsic_factory/trait.ExtrinsicBuilder.js
+++ implementors/frame_benchmarking_cli/extrinsic/extrinsic_factory/trait.ExtrinsicBuilder.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"madara":[["impl ExtrinsicBuilder for <a class=\"struct\" href=\"madara/benchmarking/struct.RemarkBuilder.html\" title=\"struct madara::benchmarking::RemarkBuilder\">RemarkBuilder</a>"],["impl ExtrinsicBuilder for <a class=\"struct\" href=\"madara/benchmarking/struct.TransferKeepAliveBuilder.html\" title=\"struct madara::benchmarking::TransferKeepAliveBuilder\">TransferKeepAliveBuilder</a>"]]
+"madara":[["impl ExtrinsicBuilder for <a class=\"struct\" href=\"madara/benchmarking/struct.RemarkBuilder.html\" title=\"struct madara::benchmarking::RemarkBuilder\">RemarkBuilder</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
