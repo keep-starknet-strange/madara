03a6e9e113f3df0a0442c2199556de259af908cd
lambda-0x <0xlambda@protonmail.com>
2023-06-10 15:33:06 +0000

    fix(CI): Dont log messages to stdout & use madara with log level error (#609)

--- scripts/run_node.sh
+++ scripts/run_node.sh
@@ -2,4 +2,5 @@
 # This script is meant to be run on Unix/Linux based systems
 set -e
 
-exec ../target/release/madara --dev --rpc-external --execution native --pool-limit=100000 --pool-kbytes=500000 --rpc-methods=unsafe --rpc-cors=all --in-peers=0 --out-peers=1 --no-telemetry
+log_level=${1:-info}
+exec ../target/release/madara --dev --rpc-external --execution native --pool-limit=100000 --pool-kbytes=500000 --rpc-methods=unsafe --rpc-cors=all --in-peers=0 --out-peers=1 --no-telemetry -l $log_level
