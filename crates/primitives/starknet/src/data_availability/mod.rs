b62e185d40097a45e58d3bb7938665360e6dda5f
Ben Goebel <dontpanicdao@gmail.com>
2023-06-12 15:53:31 -0600

    redundant import

--- crates/primitives/starknet/src/data_availability/mod.rs
+++ crates/primitives/starknet/src/data_availability/mod.rs
@@ -5,6 +5,7 @@ use async_trait::async_trait;
 
 /// In the short term we will use the lambda service to submit the OS execution trace
 pub const TESTNET_SHARP_ADDRESS: &str = "https://testnet.provingservice.io";
+pub const TESTNET_VERIFIER_ADDRESS: &str = "0x8f97970aC5a9aa8D130d35146F5b59c4aef57963";
 pub const STEP_LIMIT: u32 = 1_000_000;
 
 #[async_trait]
