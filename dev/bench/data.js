5fa825b01e4e7a0281b664c9526c491e2a0a682f
github-action-benchmark <github@users.noreply.github.com>
2023-06-13 09:25:05 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c6f67248672f3ffd7492ae9f423e57683c8be41a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686639584070,
+  "lastUpdate": 1686648305587,
   "repoUrl": "https://github.com/keep-starknet-strange/madara",
   "entries": {
     "Benchmark": [
@@ -2252,6 +2252,42 @@ window.BENCHMARK_DATA = {
             "value": 70.33333333333333,
             "unit": "tps",
             "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3615\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "70894690+LucasLvy@users.noreply.github.com",
+            "name": "Lucas @ StarkWare",
+            "username": "LucasLvy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6f67248672f3ffd7492ae9f423e57683c8be41a",
+          "message": "Refacto contract serialization (#601)",
+          "timestamp": "2023-06-13T11:21:57+02:00",
+          "tree_id": "a0c09824983a2b7aa65bd4e35cb84a7ffc45dc2e",
+          "url": "https://github.com/keep-starknet-strange/madara/commit/c6f67248672f3ffd7492ae9f423e57683c8be41a"
+        },
+        "date": 1686648305027,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Average Extrinsics per block",
+            "value": 454.5,
+            "unit": "extrinsics/block",
+            "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3000\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
+          },
+          {
+            "name": "Average TPS",
+            "value": 75.75,
+            "unit": "tps",
+            "extra": "CPU Count: 16\nCPU Model: Intel(R) Xeon(R) Platinum 8275CL CPU @ 3.00GHz\nCPU Speed (MHz): 3000\nTotal Memory: 32.822730752 GB\nPlatform: linux\nRelease: 5.19.0-1025-aws\nArchitecture: x64"
           }
         ]
       }
