30e44cd169de5545b790bf912d03d2a1e8ba1a43
lambda-0x <0xlambda@protonmail.com>
2023-06-14 18:53:19 +0000

    remove duplication

--- crates/client/rpc-core/src/utils.rs
+++ crates/client/rpc-core/src/utils.rs
@@ -83,27 +83,22 @@ pub fn to_tx(
 /// Returns a [Result<LegacyEntryPointsByType>] (blockifier type) from a [EntrypointMapWrapper]
 /// (internal type)
 fn to_legacy_entry_points_by_type(entries: &EntrypointMapWrapper) -> Result<LegacyEntryPointsByType> {
-    let constructor = entries.0
-        .get(&EntryPointTypeWrapper::Constructor).ok_or(anyhow!("Missing constructor entry point"))? // TODO: change to StarknetError
-        .iter()
-        .map(|e| (e.clone()).try_into())
-        .collect::<Result<Vec<LegacyContractEntryPoint>, FromByteArrayError>>()?;
+    fn collect_entry_points(
+        entries: &EntrypointMapWrapper,
+        entry_point_type: EntryPointTypeWrapper,
+    ) -> Result<Vec<LegacyContractEntryPoint>> {
+        Ok(entries
+            .0
+            .get(&entry_point_type)
+            .ok_or(anyhow!("Missing {:?} entry point", entry_point_type))?
+            .iter()
+            .map(|e| (e.clone()).try_into())
+            .collect::<Result<Vec<LegacyContractEntryPoint>, FromByteArrayError>>()?)
+    }
 
-    let external = entries
-        .0
-        .get(&EntryPointTypeWrapper::External)
-        .ok_or(anyhow!("Missing external entry point"))?
-        .iter()
-        .map(|e| (e.clone()).try_into())
-        .collect::<Result<Vec<LegacyContractEntryPoint>, FromByteArrayError>>()?;
-
-    let l1_handler = entries
-        .0
-        .get(&EntryPointTypeWrapper::L1Handler)
-        .ok_or(anyhow!("Missing l1 handler entry point"))?
-        .iter()
-        .map(|e| (e.clone()).try_into())
-        .collect::<Result<Vec<LegacyContractEntryPoint>, FromByteArrayError>>()?;
+    let constructor = collect_entry_points(entries, EntryPointTypeWrapper::Constructor)?;
+    let external = collect_entry_points(entries, EntryPointTypeWrapper::External)?;
+    let l1_handler = collect_entry_points(entries, EntryPointTypeWrapper::L1Handler)?;
 
     Ok(LegacyEntryPointsByType { constructor, external, l1_handler })
 }
