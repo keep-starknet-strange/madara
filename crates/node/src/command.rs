2398d1a83540af106cbf63116b25dd7cac88e5f5
shamb0 <r.raajey@gmail.com>
2023-06-20 20:24:42 +0530

    [Feature] try-runtime bring-up
    Signed-off-by: shamb0 <r.raajey@gmail.com>

--- crates/node/src/command.rs
+++ crates/node/src/command.rs
@@ -151,14 +151,26 @@ pub fn run() -> sc_cli::Result<()> {
         }
         #[cfg(feature = "try-runtime")]
         Some(Subcommand::TryRuntime(cmd)) => {
+            use sc_executor::sp_wasm_interface::ExtendedHostFunctions;
+            use sc_executor::NativeExecutionDispatch;
+            use try_runtime_cli::block_building_info::substrate_info;
+
             let runner = cli.create_runner(cmd)?;
+
+            type HostFunctionsOf<E> = ExtendedHostFunctions<
+                sp_io::SubstrateHostFunctions,
+                <E as NativeExecutionDispatch>::ExtendHostFunctions,
+            >;
+
+            let info_provider = substrate_info(madara_runtime::SLOT_DURATION);
+
             runner.async_run(|config| {
                 // we don't need any of the components of new_partial, just a runtime, or a task
                 // manager to do `async_run`.
                 let registry = config.prometheus_config.as_ref().map(|cfg| &cfg.registry);
                 let task_manager = sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
                     .map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
-                Ok((cmd.run::<Block, service::ExecutorDispatch>(config), task_manager))
+                Ok((cmd.run::<Block, HostFunctionsOf<service::ExecutorDispatch>, _>(Some(info_provider)), task_manager))
             })
         }
         #[cfg(not(feature = "try-runtime"))]
