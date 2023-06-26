a9b601f345c30ce90331ffc60646ee08ac230eab
lambda-0x <0xlambda@protonmail.com>
2023-06-26 16:08:18 +0000

    dev: clean up the numerous #[cfg(feature=std)] flags (#756)

--- crates/primitives/starknet/src/sequencer_address/mod.rs
+++ crates/primitives/starknet/src/sequencer_address/mod.rs
@@ -33,14 +33,6 @@ impl IsFatalError for InherentError {
     }
 }
 
-#[cfg(feature = "std")]
-impl InherentError {
-    /// Try to create an instance ouf of the given identifier and data.
-    pub fn try_from(id: &InherentIdentifier, mut data: &[u8]) -> Option<Self> {
-        if id == &INHERENT_IDENTIFIER { <InherentError as Decode>::decode(&mut data).ok() } else { None }
-    }
-}
-
 /// Auxiliary trait to extract sequencer address inherent data.
 pub trait SequencerAddressInherentData {
     /// Get sequencer address inherent data.
@@ -53,62 +45,72 @@ impl SequencerAddressInherentData for InherentData {
     }
 }
 
-#[derive(Copy, Clone, Decode, Encode, sp_runtime::RuntimeDebug)]
-#[cfg(feature = "std")]
-/// The inherent data provider for sequencer address.
-pub struct InherentDataProvider {
-    /// The sequencer address field.
-    pub sequencer_address: InherentType,
-}
-
-#[cfg(feature = "std")]
-impl InherentDataProvider {
-    /// Create `Self` using the given `addr`.
-    pub fn new(addr: InherentType) -> Self {
-        Self { sequencer_address: addr }
-    }
-
-    /// Returns the sequencer address of this inherent data provider.
-    pub fn sequencer_address(&self) -> InherentType {
-        self.sequencer_address
-    }
-}
-
-#[cfg(feature = "std")]
-impl Default for InherentDataProvider {
-    fn default() -> InherentDataProvider {
-        InherentDataProvider { sequencer_address: DEFAULT_SEQUENCER_ADDRESS }
-    }
-}
-
-#[cfg(feature = "std")]
-impl TryFrom<Vec<u8>> for InherentDataProvider {
-    type Error = InherentError;
-    fn try_from(storage_val: Vec<u8>) -> Result<Self, InherentError> {
-        match slice_to_arr(&storage_val) {
-            Ok(addr) => Ok(InherentDataProvider { sequencer_address: addr }),
-            Err(_) => Err(InherentError::WrongAddressFormat),
-        }
-    }
-}
-
 /// Helper function to convert storage value.
 fn slice_to_arr(slice: &[u8]) -> Result<[u8; 32], TryFromSliceError> {
     slice.try_into()
 }
 
 #[cfg(feature = "std")]
-#[async_trait::async_trait]
-impl sp_inherents::InherentDataProvider for InherentDataProvider {
-    async fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), sp_inherents::Error> {
-        inherent_data.put_data(INHERENT_IDENTIFIER, &self.sequencer_address)
+mod reexport_std_types {
+    use super::*;
+
+    impl InherentError {
+        /// Try to create an instance ouf of the given identifier and data.
+        pub fn try_from(id: &InherentIdentifier, mut data: &[u8]) -> Option<Self> {
+            if id == &INHERENT_IDENTIFIER { <InherentError as Decode>::decode(&mut data).ok() } else { None }
+        }
     }
 
-    async fn try_handle_error(
-        &self,
-        identifier: &InherentIdentifier,
-        error: &[u8],
-    ) -> Option<Result<(), sp_inherents::Error>> {
-        Some(Err(sp_inherents::Error::Application(Box::from(InherentError::try_from(identifier, error)?))))
+    #[derive(Copy, Clone, Decode, Encode, sp_runtime::RuntimeDebug)]
+    /// The inherent data provider for sequencer address.
+    pub struct InherentDataProvider {
+        /// The sequencer address field.
+        pub sequencer_address: InherentType,
+    }
+
+    impl InherentDataProvider {
+        /// Create `Self` using the given `addr`.
+        pub fn new(addr: InherentType) -> Self {
+            Self { sequencer_address: addr }
+        }
+
+        /// Returns the sequencer address of this inherent data provider.
+        pub fn sequencer_address(&self) -> InherentType {
+            self.sequencer_address
+        }
+    }
+
+    impl Default for InherentDataProvider {
+        fn default() -> InherentDataProvider {
+            InherentDataProvider { sequencer_address: DEFAULT_SEQUENCER_ADDRESS }
+        }
+    }
+
+    impl TryFrom<Vec<u8>> for InherentDataProvider {
+        type Error = InherentError;
+        fn try_from(storage_val: Vec<u8>) -> Result<Self, InherentError> {
+            match slice_to_arr(&storage_val) {
+                Ok(addr) => Ok(InherentDataProvider { sequencer_address: addr }),
+                Err(_) => Err(InherentError::WrongAddressFormat),
+            }
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl sp_inherents::InherentDataProvider for InherentDataProvider {
+        async fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), sp_inherents::Error> {
+            inherent_data.put_data(INHERENT_IDENTIFIER, &self.sequencer_address)
+        }
+
+        async fn try_handle_error(
+            &self,
+            identifier: &InherentIdentifier,
+            error: &[u8],
+        ) -> Option<Result<(), sp_inherents::Error>> {
+            Some(Err(sp_inherents::Error::Application(Box::from(InherentError::try_from(identifier, error)?))))
+        }
     }
 }
+
+#[cfg(feature = "std")]
+pub use reexport_std_types::*;
