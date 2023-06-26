45adb992b106a043c7f6aee94df6dede60d4fc2e
lambda-0x <0xlambda@protonmail.com>
2023-06-26 15:24:14 +0000

    dev: clean up the numerous #[cfg(feature=std)] flags

--- crates/primitives/starknet/src/transaction/utils.rs
+++ crates/primitives/starknet/src/transaction/utils.rs
@@ -1,28 +1,32 @@
 use alloc::vec::Vec;
-#[cfg(feature = "std")]
-use std::collections::HashMap;
-
-#[cfg(feature = "std")]
-use starknet_core::types::{LegacyContractEntryPoint, LegacyEntryPointsByType};
 
 use crate::execution::types::{EntryPointTypeWrapper, EntryPointWrapper};
 
-/// Returns a [HashMap<EntryPointTypeWrapper, Vec<EntryPointWrapper>>] from
-/// [LegacyEntryPointsByType]
 #[cfg(feature = "std")]
-pub fn to_hash_map_entrypoints(
-    entries: LegacyEntryPointsByType,
-) -> HashMap<EntryPointTypeWrapper, Vec<EntryPointWrapper>> {
-    let mut entry_points_by_type = HashMap::default();
+mod reexport_std_types {
+    use std::collections::HashMap;
 
-    entry_points_by_type.insert(EntryPointTypeWrapper::Constructor, get_entrypoint_value(entries.constructor));
-    entry_points_by_type.insert(EntryPointTypeWrapper::External, get_entrypoint_value(entries.external));
-    entry_points_by_type.insert(EntryPointTypeWrapper::L1Handler, get_entrypoint_value(entries.l1_handler));
-    entry_points_by_type
+    use starknet_core::types::{LegacyContractEntryPoint, LegacyEntryPointsByType};
+
+    use super::*;
+    /// Returns a [HashMap<EntryPointTypeWrapper, Vec<EntryPointWrapper>>] from
+    /// [LegacyEntryPointsByType]
+    pub fn to_hash_map_entrypoints(
+        entries: LegacyEntryPointsByType,
+    ) -> HashMap<EntryPointTypeWrapper, Vec<EntryPointWrapper>> {
+        let mut entry_points_by_type = HashMap::default();
+
+        entry_points_by_type.insert(EntryPointTypeWrapper::Constructor, get_entrypoint_value(entries.constructor));
+        entry_points_by_type.insert(EntryPointTypeWrapper::External, get_entrypoint_value(entries.external));
+        entry_points_by_type.insert(EntryPointTypeWrapper::L1Handler, get_entrypoint_value(entries.l1_handler));
+        entry_points_by_type
+    }
+
+    /// Returns a [Vec<EntryPointWrapper>] from a [Vec<LegacyContractEntryPoint>]
+    fn get_entrypoint_value(entries: Vec<LegacyContractEntryPoint>) -> Vec<EntryPointWrapper> {
+        entries.iter().map(|e| EntryPointWrapper::from(e.clone())).collect::<Vec<_>>()
+    }
 }
 
-/// Returns a [Vec<EntryPointWrapper>] from a [Vec<LegacyContractEntryPoint>]
 #[cfg(feature = "std")]
-fn get_entrypoint_value(entries: Vec<LegacyContractEntryPoint>) -> Vec<EntryPointWrapper> {
-    entries.iter().map(|e| EntryPointWrapper::from(e.clone())).collect::<Vec<_>>()
-}
+pub use reexport_std_types::*;
