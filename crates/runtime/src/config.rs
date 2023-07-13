bc2e75809907a1beb4de2f5d3dd047ff28794723
sparqet <sparqet@gmail.com>
2023-07-13 14:26:47 +0200

    fix bug local sync

--- crates/runtime/src/config.rs
+++ crates/runtime/src/config.rs
@@ -79,5 +79,5 @@ parameter_types! {
 
 // This storage item will be used to check if we are in the manual sealing mode
 parameter_types! {
-    pub storage EnableManualSeal: bool = false;
+    pub storage EnableManualSeal: bool = true;
 }
