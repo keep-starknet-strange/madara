0d735370de6adcd41fe62cf5a3f056a189fc3eed
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-07-31 12:36:37 +0530

    fix benchmark tests

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -105,7 +105,6 @@ mod benches {
     define_benchmarks!(
         [frame_benchmarking, BaselineBench::<Runtime>]
         [frame_system, SystemBench::<Runtime>]
-        [pallet_balances, Balances]
         [pallet_timestamp, Timestamp]
     );
 }
