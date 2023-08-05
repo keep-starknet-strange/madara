4ef82a0f8b7cd8156577b1b29897887cda4b7e5e
SFYLL <santiagoflood@hotmail.fr>
2023-08-05 09:56:30 +0200

    celestia utils

---
+++ crates/client/data-availability/src/celestia/utils.rs
@@ -0,0 +1,12 @@
+use eyre::Result;
+use celestia_types::nmt::Namespace;
+
+pub fn string_to_namespace(input: &str) -> Result<Namespace> {
+    // Convert the input string to bytes
+    let bytes = input.as_bytes();
+
+    // Create a new Namespace from these bytes
+    let namespace = Namespace::new_v0(bytes)?;
+
+    Ok(namespace)
+}
