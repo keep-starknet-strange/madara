7a639c8aff9e53c49a2d9b36f51ec52daec5f366
Evolve <artevolve@yahoo.com>
2023-08-05 19:40:48 +0200

    ✨ run node on free port

--- starknet-rpc-test/src/lib.rs
+++ starknet-rpc-test/src/lib.rs
@@ -1,10 +1,13 @@
 use std::fmt::Debug;
+use std::net::TcpListener;
 use std::path::Path;
 use std::process::{Child, Command, Stdio};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use anyhow::anyhow;
+use constants::{MAX_PORT, MIN_PORT};
 use derive_more::Display;
+use lazy_static::lazy_static;
 use reqwest::header::CONTENT_TYPE;
 use reqwest::{Client, Response};
 use serde_json::json;
@@ -13,6 +16,8 @@ use starknet_core::types::InvokeTransactionResult;
 use starknet_providers::jsonrpc::{HttpTransport, JsonRpcClient};
 use starknet_providers::Provider;
 use starknet_signers::LocalWallet;
+use thiserror::Error;
+use tokio::sync::Mutex;
 use url::Url;
 
 /// Constants (addresses, contracts...)
@@ -21,6 +26,15 @@ pub mod constants;
 pub mod utils;
 
 type TransactionExecution<'a> = Execution<'a, SingleOwnerAccount<&'a JsonRpcClient<HttpTransport>, LocalWallet>>;
+
+lazy_static! {
+        /// This is to prevent TOCTOU errors; i.e. one background madara node might find one
+        /// port to be free, and while it's trying to start listening to it, another instance
+        /// finds that it's free and tries occupying it
+        /// Using the mutex in `get_free_port_listener` might be safer than using no mutex at all,
+        /// but not sufficiently safe
+        static ref BACKGROUND_MADARA_MUTEX: Mutex<()> = Mutex::new(());
+}
 
 #[derive(Debug)]
 /// A wrapper over the Madara process handle, reqwest client and request counter
@@ -32,6 +46,7 @@ pub struct MadaraClient {
     client: Client,
     rpc_request_count: AtomicUsize,
     starknet_client: JsonRpcClient<HttpTransport>,
+    port: u16,
 }
 
 #[derive(Display)]
@@ -40,23 +55,42 @@ pub enum ExecutionStrategy {
     Wasm,
 }
 
+#[derive(Error, Debug)]
+pub enum TestError {
+    #[error("No free ports")]
+    NoFreePorts,
+}
+
 impl Drop for MadaraClient {
     fn drop(&mut self) {
-        if let Err(e) = self.process.kill() {
-            eprintln!("Could not kill Madara process: {}", e)
-        }
-        println!("dropped");
+        self.process.kill().expect("Cannot kill process");
     }
 }
 
+fn get_free_port() -> Result<u16, TestError> {
+    for port in MIN_PORT..=MAX_PORT {
+        if let Ok(listener) = TcpListener::bind(("127.0.0.1", port)) {
+            return Ok(listener.local_addr().expect("No local addr").port());
+        }
+        // otherwise port is occupied
+    }
+
+    Err(TestError::NoFreePorts)
+}
+
 impl MadaraClient {
-    fn init(execution: ExecutionStrategy) -> Self {
+    async fn init(execution: ExecutionStrategy) -> Result<Self, TestError> {
+        // we keep the reference, otherwise the mutex unlocks immediately
+        let _mutex_guard = BACKGROUND_MADARA_MUTEX.lock().await;
+
         let madara_path = Path::new("../target/release/madara");
         assert!(
             madara_path.exists(),
             "could not find the madara binary at `{}`",
             madara_path.to_str().expect("madara_path must be a valid path")
         );
+
+        let free_port = get_free_port()?;
 
         let child_handle = Command::new(madara_path.to_str().unwrap())
                 // Silence Madara stdout and stderr
@@ -67,24 +101,27 @@ impl MadaraClient {
                     "--sealing=manual",
                     &format!("--execution={execution}"),
                     "--chain=dev",
-                    "--tmp"
+                    "--tmp",
+                    &format!("--rpc-port={free_port}"),
                 ])
                 .spawn()
                 .unwrap();
 
-        let starknet_client =
-            JsonRpcClient::new(HttpTransport::new(Url::parse("http://localhost:9944").expect("Invalid JSONRPC Url")));
+        let host = &format!("http://localhost:{free_port}");
 
-        MadaraClient {
+        let starknet_client = JsonRpcClient::new(HttpTransport::new(Url::parse(host).expect("Invalid JSONRPC Url")));
+
+        Ok(MadaraClient {
             process: child_handle,
             client: Client::new(),
             starknet_client,
             rpc_request_count: Default::default(),
-        }
+            port: free_port,
+        })
     }
 
     pub async fn new(execution: ExecutionStrategy) -> Self {
-        let madara = Self::init(execution);
+        let madara = Self::init(execution).await.expect("Couldn't start Madara Node");
 
         // Wait until node is ready
         loop {
@@ -131,7 +168,7 @@ impl MadaraClient {
 
         let response = self
             .client
-            .post("http://localhost:9944")
+            .post(&format!("http://localhost:{0}", self.port))
             .header(CONTENT_TYPE, "application/json; charset=utf-8")
             .body(body)
             .send()
