8cce4d93b00c7f852ab99fde577660aa17ea23bd
Evolve <artevolve@yahoo.com>
2023-08-05 13:10:02 +0200

    📝 changelog

--- starknet-rpc-test/get_block_hash_and_number.rs
+++ starknet-rpc-test/get_block_hash_and_number.rs
@@ -16,7 +16,7 @@ async fn work_ok_at_start_and_with_new_blocks() -> Result<(), anyhow::Error> {
 
     assert_eq!(
         rpc.block_hash_and_number().await?.block_hash,
-        FieldElement::from_hex_be("0x031ebd02657f940683ae7bddf19716932c56d463fc16662d14031f8635df52ad").unwrap()
+        FieldElement::from_hex_be("0x035d394a3808b432702261c2d66f1cf2f111dd0c691ad2a8246b86a5088f73fa").unwrap()
     );
     assert_eq!(rpc.block_hash_and_number().await?.block_number, 0);
 
@@ -28,6 +28,7 @@ async fn work_ok_at_start_and_with_new_blocks() -> Result<(), anyhow::Error> {
     assert_eq!(rpc.block_hash_and_number().await?.block_number, 1);
 
     let signer = LocalWallet::from(SigningKey::from_secret_scalar(FieldElement::from_hex_be(SIGNER_PRIVATE).unwrap()));
+    let argent_account_address = FieldElement::from_hex_be(ARGENT_CONTRACT_ADDRESS).expect("Invalid Contract Address");
     let account = SingleOwnerAccount::new(rpc, signer, argent_account_address, chain_id::TESTNET);
 
     madara
