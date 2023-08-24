65f9be8bc5412da1dbd3911e69fe4343e0862469
tdelabro <tdelabro@users.noreply.github.com>
2023-08-24 18:02:11 +0000

    deploy: 1729adc39d05b7e40c3e79731a1a0cba52579ba1

--- pallet_starknet/types/sidebar-items.js
+++ pallet_starknet/types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["BuildTransferInvokeTransaction","DeployAccountTransactionOutput","StateCommitments"],"type":["ContractClassMapping","ContractStorageKeyWrapper","MaxTransactionsPendingBlock","NonceWrapper","StateTrie","StorageKeyWrapper","StorageSlotWrapper"]};
+window.SIDEBAR_ITEMS = {"struct":["BuildTransferInvokeTransaction","DeployAccountTransactionOutput"],"type":["ContractClassMapping","ContractStorageKeyWrapper","MaxTransactionsPendingBlock","NonceWrapper","StorageKeyWrapper","StorageSlotWrapper"]};
