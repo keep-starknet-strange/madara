8a17b8c3cb08ab7e37ae1310773dfdbbc654ec87
d-roak <d-roak@users.noreply.github.com>
2023-09-07 06:02:38 +0000

    deploy: 2097ec2b6c71fc0b27d0f86d8fe9497ff599359f

--- implementors/sp_inherents/client_side/trait.InherentDataProvider.js
+++ implementors/sp_inherents/client_side/trait.InherentDataProvider.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"madara":[["impl InherentDataProvider for <a class=\"struct\" href=\"madara/service/struct.MockTimestampInherentDataProvider.html\" title=\"struct madara::service::MockTimestampInherentDataProvider\">MockTimestampInherentDataProvider</a>"]],
 "mp_starknet":[["impl InherentDataProvider for <a class=\"struct\" href=\"mp_starknet/sequencer_address/struct.InherentDataProvider.html\" title=\"struct mp_starknet::sequencer_address::InherentDataProvider\">InherentDataProvider</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
