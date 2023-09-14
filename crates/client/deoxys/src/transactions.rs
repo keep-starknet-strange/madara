5dbd1f20dfc6130ad1dbe12b32406150af765629
lucienfer <lucien.fernandez02@gmail.com>
2023-09-14 13:58:50 +0200

    version and sender_address add

--- crates/client/deoxys/src/transactions.rs
+++ crates/client/deoxys/src/transactions.rs
@@ -15,7 +15,6 @@ pub fn declare_tx_to_starknet_tx(declare_transaction: IntermediateDeclareTransac
                 panic!("Signature too long");
             }
         }
-        signature_vec.try_push(Felt252Wrapper::try_from(item.bytes()).unwrap());
     }
     let calldata_vec: BoundedVec<Felt252Wrapper, MaxCalldataSize> = BoundedVec::new();
 
@@ -30,25 +29,23 @@ pub fn declare_tx_to_starknet_tx(declare_transaction: IntermediateDeclareTransac
         Some(ClassHashWrapper::ZERO)
     );
 
-    // let version_byte: [u8; 32] = match Felt252Wrapper::try_from(declare_transaction.version.0.into()) {
-    //     Ok(valeur) => {
-    //         Felt252Wrapper::from(valeur).into()
-    //     },
-    //     Err(_) => {panic!("Version too long")}
-    // };
-    // let version_u8: u8 = match version_byte[0] {
-    //     0 => 0,
-    //     1 => 1,
-    //     01 => 2,
-    //     _ => panic!("Version not supported")
-    // };
+    let version_fw: Felt252Wrapper = Felt252Wrapper(FieldElement::from(declare_transaction.version.0));
+    let version_u8: u8 = match u64::try_from(version_fw) {
+        Ok(valeur) => {
+            valeur as u8
+        },
+        Err(_) => {panic!("Version too long")}
+    };
+
+    let sender_address_fe: FieldElement =  FieldElement::from(*PatriciaKey::key(&declare_transaction.sender_address.0));
+    let sender_address_fw: Felt252Wrapper = Felt252Wrapper(sender_address_fe);
     
     Transaction {
         tx_type: TxType::Declare,
-        version: Some(u8::default()).unwrap(),
+        version: version_u8,
         hash: Felt252Wrapper(declare_transaction.transaction_hash.0.into()),
         signature: signature_vec,
-        sender_address: Some(Felt252Wrapper::default()).unwrap(),
+        sender_address: sender_address_fw,
         nonce: Felt252Wrapper::try_from(declare_transaction.nonce.0.bytes()).unwrap(),
         call_entrypoint: call_entry_point,
         contract_class: Option::<ContractClass>::default(),
@@ -133,13 +130,24 @@ pub fn invoke_tx_to_starknet_tx(invoke_transaction : IntermediateInvokeTransacti
 //         Felt252Wrapper::ZERO,
 //         Some(ClassHashWrapper::ZERO)
 //     );
-    
+
+//        let version_fw: Felt252Wrapper = Felt252Wrapper(FieldElement::from(deploy_transaction.version.0));
+//        let version_u8: u8 = match u64::try_from(version_fw) {
+//            Ok(valeur) => {
+//                valeur as u8
+//            },
+//            Err(_) => {panic!("Version too long")}
+//        };
+//
+//      let sender_address_fe: FieldElement =  FieldElement::from(*PatriciaKey::key(&deploy_transaction.sender_address.0));
+//      let sender_address_fw: Felt252Wrapper = Felt252Wrapper(sender_address_fe);
+
 //     Transaction {
 //         tx_type: TxType::Deploy,
-//         version: b'1',
+//         version: version_u8,
 //         hash: Felt252Wrapper(deploy_transaction.transaction_hash.0.into()),
 //         signature: signature_vec,
-//         sender_address: Some(Felt252Wrapper::default()).unwrap(),
+//         sender_address: sender_address_fw,
 //         nonce: Some(Felt252Wrapper::default()).unwrap(),
 //         call_entrypoint: call_entry_point,
 //         contract_class: Option::<ContractClass>::default(),
@@ -172,13 +180,24 @@ pub fn deploy_account_tx_to_starknet_tx(mut deploy_account_transaction : DeployA
         Felt252Wrapper::ZERO,
         Some(ClassHashWrapper::ZERO)
     );
-    
+
+    let version_fw: Felt252Wrapper = Felt252Wrapper(FieldElement::from(deploy_account_transaction.version.0));
+    let version_u8: u8 = match u64::try_from(version_fw) {
+        Ok(valeur) => {
+            valeur as u8
+        },
+        Err(_) => {panic!("Version too long")}
+    };
+
+    //let sender_address_fe: FieldElement =  FieldElement::from(*PatriciaKey::key(&deploy_account_transaction.sender_address.0));
+    //let sender_address_fw: Felt252Wrapper = Felt252Wrapper(sender_address_fe);
+
     Transaction {
         tx_type: TxType::DeployAccount,
-        version: b'1',
+        version: version_u8,
         hash: Felt252Wrapper(deploy_account_transaction.transaction_hash.0.into()),
         signature: signature_vec,
-        sender_address: Some(Felt252Wrapper::default()).unwrap(),
+        sender_address: Felt252Wrapper::default(),
         nonce: Felt252Wrapper::try_from(deploy_account_transaction.nonce.0.bytes()).unwrap(),
         call_entrypoint: call_entry_point,
         contract_class: Option::<ContractClass>::default(),
@@ -211,13 +230,24 @@ pub fn l1handler_tx_to_starknet_tx(mut l1handler_transaction : L1HandlerTransact
         Felt252Wrapper::ZERO,
         Some(ClassHashWrapper::ZERO)
     );
-    
+
+    let version_fw: Felt252Wrapper = Felt252Wrapper(FieldElement::from(l1handler_transaction.version.0));
+    let version_u8: u8 = match u64::try_from(version_fw) {
+        Ok(valeur) => {
+            valeur as u8
+        },
+        Err(_) => {panic!("Version too long")}
+    };
+
+    //let sender_address_fe: FieldElement =  FieldElement::from(*PatriciaKey::key(&l1handler_transaction.sender_address.0));
+    //let sender_address_fw: Felt252Wrapper = Felt252Wrapper(sender_address_fe);
+
     Transaction {
         tx_type: TxType::L1Handler,
-        version: b'1',
+        version: version_u8,
         hash: Felt252Wrapper(l1handler_transaction.transaction_hash.0.into()),
         signature: signature_vec,
-        sender_address: Some(Felt252Wrapper::default()).unwrap(),
+        sender_address: Felt252Wrapper::default(),
         nonce: Felt252Wrapper::try_from(l1handler_transaction.nonce.0.bytes()).unwrap(),
         call_entrypoint: call_entry_point,
         contract_class: Option::<ContractClass>::default(),
