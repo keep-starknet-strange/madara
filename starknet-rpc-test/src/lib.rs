2b42aadc221a7f010c322f20eea9e6beeb24b137
d-roak <me+git@droak.sh>
2023-10-01 02:20:36 +0900

    fmt

--- starknet-rpc-test/src/lib.rs
+++ starknet-rpc-test/src/lib.rs
@@ -170,16 +170,11 @@ impl MadaraClient {
         let madara_log = std::env::var("MADARA_LOG").unwrap_or_else(|_| "false".to_string());
 
         Command::new("cargo")
-		.stdout(Stdio::null())
-		.stderr(if madara_log == "true" { Stdio::inherit() } else { Stdio::null() })
-		.args([
-			"run",
-			"--release",
-			"--",
-			"setup",
-			])
-			.spawn()
-			.expect("Could not setup madara node");
+            .stdout(Stdio::null())
+            .stderr(if madara_log == "true" { Stdio::inherit() } else { Stdio::null() })
+            .args(["run", "--release", "--", "setup"])
+            .spawn()
+            .expect("Could not setup madara node");
 
         let child_handle = Command::new("cargo")
 		// Silence Madara stdout and stderr
