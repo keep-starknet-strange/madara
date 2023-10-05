25320d4c0c1e7e0056319b4d5adf08fa446a828a
Daniel Bigos <daniel.bigos@icloud.com>
2023-10-05 15:41:38 +0200

    test: check valid unsigned L1 tx attributes
    Refers #1163

--- crates/pallets/starknet/src/tests/l1_message.rs
+++ crates/pallets/starknet/src/tests/l1_message.rs
@@ -1,46 +1,19 @@
-use frame_support::assert_err;
 use mp_felt::Felt252Wrapper;
-use mp_transactions::{DeclareTransactionV1, HandleL1MessageTransaction};
+use mp_transactions::HandleL1MessageTransaction;
 use sp_runtime::traits::ValidateUnsigned;
-use sp_runtime::transaction_validity::TransactionSource;
+use sp_runtime::transaction_validity::{TransactionSource, TransactionTag};
+use starknet_api::api_core::ContractAddress;
 use starknet_api::transaction::Fee;
 
 use super::mock::default_mock::*;
 use super::mock::*;
-use super::utils::get_contract_class;
-use crate::Error;
+
+use frame_support::codec::Encode;
+
+const VALID_TX_BUILDER_TAG: &str = "starknet";
 
 #[test]
-fn given_contract_l1_message_fails_sender_not_deployed() {
-    new_test_ext::<MockRuntime>().execute_with(|| {
-        basic_test_setup(2);
-
-        let none_origin = RuntimeOrigin::none();
-
-        // Wrong address (not deployed)
-        let contract_address =
-            Felt252Wrapper::from_hex_be("0x03e437FB56Bb213f5708Fcd6966502070e276c093ec271aA33433b89E21fd31f").unwrap();
-
-        let erc20_class = get_contract_class("ERC20.json", 0);
-
-        let transaction = DeclareTransactionV1 {
-            sender_address: contract_address,
-            nonce: Default::default(),
-            signature: Default::default(),
-            max_fee: Default::default(),
-            class_hash: Default::default(),
-        };
-
-        assert_err!(
-            Starknet::declare(none_origin, transaction.into(), erc20_class),
-            Error::<MockRuntime>::AccountNotDeployed
-        );
-    })
-}
-
-#[test]
-#[ignore = "l1 handler validation not implemented yet"]
-fn verify_tx_longevity() {
+fn verify_tx_validity() {
     new_test_ext::<MockRuntime>().execute_with(|| {
         basic_test_setup(2);
 
@@ -51,11 +24,24 @@ fn verify_tx_longevity() {
             calldata: Default::default(),
         };
 
+        let expected_priority = u64::MAX - transaction.nonce;
+        let expected_provide: (Felt252Wrapper, Felt252Wrapper) = (ContractAddress::default().into(), transaction.nonce.into());
+		let expected_provide: TransactionTag = (VALID_TX_BUILDER_TAG, expected_provide).encode();
+        let expected_longetivity = TransactionLongevity::get();
+        let expected_propagate = true;
+
         let validate_result = Starknet::validate_unsigned(
             TransactionSource::InBlock,
             &crate::Call::consume_l1_message { transaction, paid_fee_on_l1: Fee(100) },
         );
 
-        assert!(validate_result.unwrap().longevity == TransactionLongevity::get());
+        assert!(validate_result.is_ok());
+        let validate_result = validate_result.unwrap();
+
+		assert_eq!(validate_result.priority, expected_priority);
+		assert_eq!(validate_result.requires, Vec::<TransactionTag>::new());
+        assert_eq!(validate_result.provides, Vec::<TransactionTag>::from([expected_provide]));
+		assert_eq!(validate_result.longevity, expected_longetivity);
+		assert_eq!(validate_result.propagate, expected_propagate);
     });
 }
