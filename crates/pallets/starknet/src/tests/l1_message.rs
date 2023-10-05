2e058ae3ddd39e75543802f536117ed5e2752028
Daniel Bigos <daniel.bigos@icloud.com>
2023-10-05 19:08:11 +0200

    test: tests for unsigned L1 Message transactions
    Refers #1163

--- crates/pallets/starknet/src/tests/l1_message.rs
+++ crates/pallets/starknet/src/tests/l1_message.rs
@@ -1,14 +1,15 @@
+use frame_support::codec::Encode;
 use mp_felt::Felt252Wrapper;
 use mp_transactions::HandleL1MessageTransaction;
 use sp_runtime::traits::ValidateUnsigned;
-use sp_runtime::transaction_validity::{TransactionSource, TransactionTag};
-use starknet_api::api_core::ContractAddress;
+use sp_runtime::transaction_validity::{TransactionSource, TransactionTag, TransactionValidityError};
+use starknet_api::api_core::{ContractAddress, Nonce};
+use starknet_api::hash::StarkFelt;
 use starknet_api::transaction::Fee;
 
 use super::mock::default_mock::*;
 use super::mock::*;
-
-use frame_support::codec::Encode;
+use crate::{Call, InvalidTransaction, L1Messages};
 
 const VALID_TX_BUILDER_TAG: &str = "starknet";
 
@@ -25,23 +26,52 @@ fn verify_tx_validity() {
         };
 
         let expected_priority = u64::MAX - transaction.nonce;
-        let expected_provide: (Felt252Wrapper, Felt252Wrapper) = (ContractAddress::default().into(), transaction.nonce.into());
-		let expected_provide: TransactionTag = (VALID_TX_BUILDER_TAG, expected_provide).encode();
+        let expected_provide: (Felt252Wrapper, Felt252Wrapper) =
+            (ContractAddress::default().into(), transaction.nonce.into());
+        let expected_provide: TransactionTag = (VALID_TX_BUILDER_TAG, expected_provide).encode();
         let expected_longetivity = TransactionLongevity::get();
         let expected_propagate = true;
 
         let validate_result = Starknet::validate_unsigned(
             TransactionSource::InBlock,
-            &crate::Call::consume_l1_message { transaction, paid_fee_on_l1: Fee(100) },
+            &Call::consume_l1_message { transaction, paid_fee_on_l1: Fee(100) },
         );
 
         assert!(validate_result.is_ok());
         let validate_result = validate_result.unwrap();
 
-		assert_eq!(validate_result.priority, expected_priority);
-		assert_eq!(validate_result.requires, Vec::<TransactionTag>::new());
+        assert_eq!(validate_result.priority, expected_priority);
+        assert_eq!(validate_result.requires, Vec::<TransactionTag>::new());
         assert_eq!(validate_result.provides, Vec::<TransactionTag>::from([expected_provide]));
-		assert_eq!(validate_result.longevity, expected_longetivity);
-		assert_eq!(validate_result.propagate, expected_propagate);
+        assert_eq!(validate_result.longevity, expected_longetivity);
+        assert_eq!(validate_result.propagate, expected_propagate);
+    });
+}
+
+#[test]
+fn should_reject_used_nonce() {
+    new_test_ext::<MockRuntime>().execute_with(|| {
+        basic_test_setup(2);
+
+        let nonce: u64 = 1;
+
+        let transaction = HandleL1MessageTransaction {
+            nonce,
+            contract_address: Default::default(),
+            entry_point_selector: Default::default(),
+            calldata: Default::default(),
+        };
+
+        let tx_source = TransactionSource::InBlock;
+        let call = Call::consume_l1_message { transaction, paid_fee_on_l1: Fee(100) };
+
+        assert!(Starknet::validate_unsigned(tx_source, &call).is_ok());
+
+        L1Messages::<MockRuntime>::insert(Nonce(StarkFelt::from(nonce)), ());
+
+        assert_eq!(
+            Starknet::validate_unsigned(tx_source, &call),
+            Err(TransactionValidityError::Invalid(InvalidTransaction::Stale))
+        );
     });
 }
