e949717d7925339f9426a395b8797f85a6be1a7e
Michael Zaikin <mz@pklab.io>
2023-10-06 15:30:17 +0300

    snos codec

--- crates/primitives/snos-output/src/codec.rs
+++ crates/primitives/snos-output/src/codec.rs
@@ -1,181 +1,80 @@
 use alloc::vec::Vec;
 
-use parity_scale_codec::{Decode, Encode, Input, Output};
-use starknet_api::api_core::{ContractAddress, EntryPointSelector, EthAddress, Nonce};
-use starknet_api::hash::StarkHash;
+use starknet_api::hash::StarkFelt;
 
 use crate::{MessageL1ToL2, MessageL2ToL1, StarknetOsOutput};
 
-// Field element (252 bit) is encoded as an EVM word (256 bit) and vice versa
-// EVM developer should be aware of that and prevent data loss by not using the higest 4 bits
+/// This codec allows to convert structured OS program output into array of felts
+/// 
+/// In order to prepare parameters for the Starknet contract `updateState` method:
+///     1. Cast the output to dynamic uint256[] array
+///     2. Get onchain data hash & size
+///     3. ABI encode parameters 
+///  
+/// NOTE: Field element (252 bit) is encoded as an EVM word (256 bit) and vice versa
+/// EVM developer should be aware of that and prevent data loss by not using the higest 4 bits
+pub trait SnosCodec: Sized {
+    fn felt_len(&self) -> usize;
+    fn append_to(self, output: &mut Vec<StarkFelt>);
 
-fn skip_bytes<I: Input>(input: &mut I, count: usize) -> Result<(), parity_scale_codec::Error> {
-    for _ in 0..count {
-        input.read_byte()?;
-    }
-    Ok(())
-}
-
-fn u64_encode<O: Output + ?Sized>(value: u64, dest: &mut O) {
-    dest.write(&[0u8; 24]); // 24 bytes padding
-    dest.write(value.to_be_bytes().as_slice());
-}
-
-fn u64_decode<I: Input>(input: &mut I) -> Result<u64, parity_scale_codec::Error> {
-    skip_bytes(input, 24)?;
-    let mut bytes = [0u8; 8];
-    input.read(bytes.as_mut_slice())?;
-    Ok(u64::from_be_bytes(bytes))
-}
-
-fn vec_encode<T: Encode, O: Output + ?Sized>(items: &[T], dest: &mut O) {
-    u64_encode(items.len() as u64, dest);
-    for item in items.iter() {
-        item.encode_to(dest); // 32 bytes
+    fn as_vec(self) -> Vec<StarkFelt> {
+        let mut output: Vec<StarkFelt> = Vec::with_capacity(self.felt_len());
+        self.append_to(&mut output);
+        output
     }
 }
 
-fn vec_decode<T: Decode, I: Input>(input: &mut I) -> Result<Vec<T>, parity_scale_codec::Error> {
-    let n_items = u64_decode(input)?;
-    let mut items: Vec<T> = Vec::with_capacity(n_items as usize);
-    for _ in 0..n_items {
-        items.push(T::decode(input)?);
-    }
-    Ok(items)
-}
-
-fn segment_encode<T: Encode, O: Output + ?Sized>(items: &[T], dest: &mut O) {
-    // Number of byte32 words
-    let segment_size = items.iter().map(|item| item.size_hint() as u64).sum::<u64>() / 32;
-    u64_encode(segment_size, dest);
-    for item in items.iter() {
-        item.encode_to(dest);
+fn segment_append_to<T: SnosCodec>(segment: Vec<T>, output: &mut Vec<StarkFelt>) {
+    let segment_size = segment.iter().map(|msg| msg.felt_len() as u64).sum::<u64>();
+    output.push(segment_size.into());
+    for item in segment {
+        item.append_to(output);
     }
 }
 
-fn segment_decode<T: Encode + Decode, I: Input>(input: &mut I) -> Result<Vec<T>, parity_scale_codec::Error> {
-    let mut segment_len = 32 * u64_decode(input)? as usize;
-    let mut items: Vec<T> = Vec::with_capacity(segment_len / 3); // minimum capacity for empty l2 > l1 messages
-
-    if let Some(remaining_len) = input.remaining_len()? {
-        if segment_len > remaining_len {
-            return Err("Segment size is greater than remaining read buffer length".into());
-        }
+impl SnosCodec for StarknetOsOutput {
+    fn felt_len(&self) -> usize {
+        7 + self.messages_to_l1.iter().map(|msg| msg.felt_len()).sum::<usize>()
+            + self.messages_to_l2.iter().map(|msg| msg.felt_len()).sum::<usize>()
     }
 
-    while segment_len > 0 {
-        let item = T::decode(input)?;
-        segment_len -= item.size_hint();
-        items.push(item);
-    }
-
-    Ok(items)
-}
-
-fn eth_address_encode<O: Output + ?Sized>(address: &EthAddress, dest: &mut O) {
-    dest.write(&[0u8; 12]); // 12 bytes padding
-    address.encode_to(dest); // 20 bytes
-}
-
-fn eth_address_decode<I: Input>(input: &mut I) -> Result<EthAddress, parity_scale_codec::Error> {
-    skip_bytes(input, 12)?;
-    EthAddress::decode(input)
-}
-
-impl Encode for MessageL2ToL1 {
-    fn encode_to<O: Output + ?Sized>(&self, dest: &mut O) {
-        self.from_address.encode_to(dest);
-        eth_address_encode(&self.to_address, dest);
-        vec_encode(&self.payload, dest);
-    }
-
-    fn size_hint(&self) -> usize {
-        // from_address
-        // to_address
-        // payload_size
-        // payload
-        32 * (self.payload.len() + 3)
+    fn append_to(self, output: &mut Vec<StarkFelt>) {
+        output.push(self.prev_state_root);
+        output.push(self.new_state_root);
+        output.push(self.block_number);
+        output.push(self.block_hash);
+        output.push(self.config_hash);
+        segment_append_to(self.messages_to_l1, output);
+        segment_append_to(self.messages_to_l2, output);
     }
 }
 
-impl Decode for MessageL2ToL1 {
-    fn decode<I: Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
-        Ok(Self {
-            from_address: ContractAddress::decode(input)?,
-            to_address: eth_address_decode(input)?,
-            payload: vec_decode(input)?,
-        })
+impl SnosCodec for MessageL2ToL1 {
+    fn felt_len(&self) -> usize {
+        3 + self.payload.len()
+    }
+
+    fn append_to(self, output: &mut Vec<StarkFelt>) {
+        output.push(self.from_address);
+        output.push(self.to_address);
+        output.push((self.payload.len() as u64).into());
+        let mut payload = self.payload;
+        output.append(&mut payload);
     }
 }
 
-impl Encode for MessageL1ToL2 {
-    fn encode_to<O: Output + ?Sized>(&self, dest: &mut O) {
-        eth_address_encode(&self.from_address, dest);
-        self.to_address.encode_to(dest);
-        self.nonce.encode_to(dest);
-        self.selector.encode_to(dest);
-        vec_encode(&self.payload, dest);
+impl SnosCodec for MessageL1ToL2 {
+    fn felt_len(&self) -> usize {
+        5 + self.payload.len()
     }
 
-    fn size_hint(&self) -> usize {
-        // from_address
-        // to_address
-        // nonce
-        // selector
-        // payload_size
-        // payload
-        32 * (self.payload.len() + 5)
-    }
-}
-
-impl Decode for MessageL1ToL2 {
-    fn decode<I: Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
-        Ok(Self {
-            from_address: eth_address_decode(input)?,
-            to_address: ContractAddress::decode(input)?,
-            nonce: Nonce::decode(input)?,
-            selector: EntryPointSelector::decode(input)?,
-            payload: vec_decode(input)?,
-        })
-    }
-}
-
-impl Encode for StarknetOsOutput {
-    fn encode_to<O: Output + ?Sized>(&self, dest: &mut O) {
-        self.prev_state_root.encode_to(dest);
-        self.new_state_root.encode_to(dest);
-        u64_encode(self.block_number, dest);
-        self.block_hash.encode_to(dest);
-        self.config_hash.encode_to(dest);
-        segment_encode(&self.messages_to_l1, dest);
-        segment_encode(&self.messages_to_l2, dest);
-    }
-
-    fn size_hint(&self) -> usize {
-        // prev_state_root
-        // next_state_root
-        // block_number
-        // config_hash
-        // segment_size
-        // messages_to_l1
-        // segment_size
-        // messages_to_l2
-        32 * 6
-            + self.messages_to_l1.iter().map(|msg| msg.size_hint()).sum::<usize>()
-            + self.messages_to_l2.iter().map(|msg| msg.size_hint()).sum::<usize>()
-    }
-}
-
-impl Decode for StarknetOsOutput {
-    fn decode<I: Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
-        Ok(Self {
-            prev_state_root: StarkHash::decode(input)?,
-            new_state_root: StarkHash::decode(input)?,
-            block_number: u64_decode(input)?,
-            block_hash: StarkHash::decode(input)?,
-            config_hash: StarkHash::decode(input)?,
-            messages_to_l1: segment_decode(input)?,
-            messages_to_l2: segment_decode(input)?,
-        })
+    fn append_to(self, output: &mut Vec<StarkFelt>) {
+        output.push(self.from_address);
+        output.push(self.to_address);
+        output.push(self.nonce);
+        output.push(self.selector);
+        output.push((self.payload.len() as u64).into());
+        let mut payload = self.payload;
+        output.append(&mut payload);
     }
 }
--- crates/primitives/snos-output/src/lib.rs
+++ crates/primitives/snos-output/src/lib.rs
@@ -4,9 +4,7 @@
 #[doc(hidden)]
 pub extern crate alloc;
 
-#[cfg(feature = "parity-scale-codec")]
 mod codec;
-
 mod conversions;
 
 #[cfg(test)]
@@ -14,22 +12,23 @@ mod tests;
 
 use alloc::vec::Vec;
 
-use starknet_api::api_core::{ContractAddress, EntryPointSelector, EthAddress, Nonce};
-use starknet_api::hash::{StarkFelt, StarkHash};
+use starknet_api::hash::StarkFelt;
 
 #[derive(Clone, Debug, PartialEq, Eq, Default)]
 #[cfg_attr(feature = "scale-info", derive(scale_info::TypeInfo))]
+#[cfg_attr(feature = "parity-scale-codec", derive(parity_scale_codec::Encode, parity_scale_codec::Decode))]
+/// Main part of Starknet OS program output
 pub struct StarknetOsOutput {
     /// The state commitment before this block.
-    pub prev_state_root: StarkHash,
+    pub prev_state_root: StarkFelt,
     /// The state commitment after this block.
-    pub new_state_root: StarkHash,
+    pub new_state_root: StarkFelt,
     /// The number (height) of this block.
-    pub block_number: u64,
+    pub block_number: StarkFelt,
     /// The hash of this block.
-    pub block_hash: StarkHash,
+    pub block_hash: StarkFelt,
     /// The Starknet chain config hash
-    pub config_hash: StarkHash,
+    pub config_hash: StarkFelt,
     /// List of messages sent to L1 in this block
     pub messages_to_l1: Vec<MessageL2ToL1>,
     /// List of messages from L1 handled in this block
@@ -38,18 +37,22 @@ pub struct StarknetOsOutput {
 
 #[derive(Clone, Debug, PartialEq, Eq, Default)]
 #[cfg_attr(feature = "scale-info", derive(scale_info::TypeInfo))]
+#[cfg_attr(feature = "parity-scale-codec", derive(parity_scale_codec::Encode, parity_scale_codec::Decode))]
+/// Message sent to L1 by invoking according Statknet syscall
 pub struct MessageL2ToL1 {
-    pub from_address: ContractAddress,
-    pub to_address: EthAddress,
+    pub from_address: StarkFelt,
+    pub to_address: StarkFelt,
     pub payload: Vec<StarkFelt>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Default)]
 #[cfg_attr(feature = "scale-info", derive(scale_info::TypeInfo))]
+#[cfg_attr(feature = "parity-scale-codec", derive(parity_scale_codec::Encode, parity_scale_codec::Decode))]
+/// Message sent to L2 by calling Starknet smart contract on Ethereum
 pub struct MessageL1ToL2 {
-    pub from_address: EthAddress,
-    pub to_address: ContractAddress,
-    pub nonce: Nonce,
-    pub selector: EntryPointSelector,
+    pub from_address: StarkFelt,
+    pub to_address: StarkFelt,
+    pub nonce: StarkFelt,
+    pub selector: StarkFelt,
     pub payload: Vec<StarkFelt>,
 }
