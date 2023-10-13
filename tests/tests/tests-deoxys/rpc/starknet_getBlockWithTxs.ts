455f0dcbad4865f4f53925516d6fb1e5c929ae5f
antiyro <antiyro@gmail.com>
2023-10-13 10:45:30 +0200

    added deploy versions

--- tests/tests/tests-deoxys/rpc/starknet_getBlockWithTxs.ts
+++ tests/tests/tests-deoxys/rpc/starknet_getBlockWithTxs.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 const REMOTE_RPC_URL = process.env.REMOTE_RPC;
 const LOCAL_RPC_URL = process.env.DEOXYS_RPC;
-const BLOCK_NUMBER = 2000;
-const START_BLOCK = 2000;
-const END_BLOCK = 2020;
+const BLOCK_NUMBER = 5066;
+const START_BLOCK = 5000;
+const END_BLOCK = 15000;
 
 const requestDataForMethod = (method: string, params: any[]) => ({
   id: 1,
@@ -49,20 +49,20 @@ const compareObjects = (obj1: any, obj2: any, path: string = ""): string => {
 };
 
 async function benchmarkMethod(method: string, params: any[]): Promise<string> {
-  console.log(
-    `\x1b[34mBenchmarking method: ${method}\x1b[0m for block_number: ${params[0].block_number}`,
-  );
+  console.log(`\x1b[34mBenchmarking method: ${method}\x1b[0m for block_number: ${params[0].block_number}`);
 
-  const alchemyResponse = await axios.post(
-    REMOTE_RPC_URL,
-    requestDataForMethod(method, params),
-  );
-  const localResponse = await axios.post(
-    LOCAL_RPC_URL,
-    requestDataForMethod(method, params),
-  );
+  const alchemyResponse = await axios.post(REMOTE_RPC_URL, requestDataForMethod(method, params));
+  const localResponse = await axios.post(LOCAL_RPC_URL, requestDataForMethod(method, params));
 
-  return compareObjects(alchemyResponse.data, localResponse.data);
+  const differences = compareObjects(alchemyResponse.data, localResponse.data);
+
+  if (differences.includes("\x1b[31mDIFFERENCE")) {
+      console.log(`\x1b[31mBlock ${params[0].block_number} has differences.\x1b[0m`);
+  } else {
+      console.log(`\x1b[32mBlock ${params[0].block_number} matches.\x1b[0m`);
+  }
+
+  return differences;
 }
 
 async function checkDifferencesInBlocks() {
@@ -81,10 +81,7 @@ async function checkDifferencesInBlocks() {
   if (blocksWithDifferences.length === 0) {
     console.log("\x1b[32mAll blocks match!\x1b[0m");
   } else {
-    console.log(
-      "\x1b[31mDifferences found in blocks:\x1b[0m",
-      blocksWithDifferences,
-    );
+    console.log("\x1b[31mDifferences found in blocks:\x1b[0m", JSON.stringify(blocksWithDifferences));
   }
 }
 
