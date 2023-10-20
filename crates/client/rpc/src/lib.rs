keep-starknet-strange/madara

commit 645d9f0afac104be3bea2f3fa2a1ec57900c3a9d

feat: add `--cache` option, allows caching more information into the database (#1197)

crates/client/rpc/src/lib.rs
@@ -157,6 +157,18 @@ where
 
         Ok(block.header().block_number)
     }
+
+    /// Returns a list of all transaction hashes in the given block.
+    ///
+    /// # Arguments
+    ///
+    /// * `block_hash` - The hash of the block containing the transactions (starknet block).
+    fn get_cached_transaction_hashes(&self, block_hash: H256) -> Option<Vec<H256>> {
+        self.backend.mapping().cached_transaction_hashes_from_block_hash(block_hash).unwrap_or_else(|err| {
+            error!("{err}");
+            None
+        })
+    }
 }
 
 /// Taken from https://github.com/paritytech/substrate/blob/master/client/rpc/src/author/mod.rs#L78
@@ -401,13 +413,21 @@ where
 
         let block = get_block_by_block_hash(self.client.as_ref(), substrate_block_hash).unwrap_or_default();
         let chain_id = self.chain_id()?;
-
-        let transactions =
-            block.transactions_hashes::<H>(Felt252Wrapper(chain_id.0)).into_iter().map(FieldElement::from).collect();
         let blockhash = block.header().hash::<H>();
+
+        let transaction_hashes = if let Some(tx_hashes) = self.get_cached_transaction_hashes(blockhash.into()) {
+            let mut v = Vec::with_capacity(tx_hashes.len());
+            for tx_hash in tx_hashes {
+                v.push(h256_to_felt(tx_hash)?);
+            }
+            v
+        } else {
+            block.transactions_hashes::<H>(chain_id.0.into()).map(FieldElement::from).collect()
+        };
+
         let parent_blockhash = block.header().parent_block_hash;
         let block_with_tx_hashes = BlockWithTxHashes {
-            transactions,
+            transactions: transaction_hashes,
             // TODO: Status hardcoded, get status from block
             status: BlockStatus::AcceptedOnL2,
             block_hash: blockhash.into(),
@@ -634,7 +654,12 @@ where
         let transaction = block.transactions().get(index as usize).ok_or(StarknetRpcApiError::InvalidTxnIndex)?;
         let chain_id = self.chain_id()?;
 
-        Ok(to_starknet_core_tx::<H>(transaction.clone(), Felt252Wrapper(chain_id.0)))
+        let transaction_hash = self
+            .get_cached_transaction_hashes(block.header().hash::<H>().into())
+            .map(|tx_hashes| h256_to_felt(*tx_hashes.get(index as usize).ok_or(StarknetRpcApiError::InvalidTxnIndex)?))
+            .unwrap_or_else(|| Ok(transaction.compute_hash::<H>(chain_id.0.into(), false).0))?;
+
+        Ok(to_starknet_core_tx::<H>(transaction.clone(), transaction_hash))
     }
 
     /// Get block information with full transactions given the block id
@@ -649,6 +674,16 @@ where
         let chain_id = self.chain_id()?;
         let chain_id = Felt252Wrapper(chain_id.0);
 
+        let transaction_hashes = self.get_cached_transaction_hashes(block.header().hash::<H>().into());
+        let mut transactions = Vec::with_capacity(block.transactions().len());
+        for (index, tx) in block.transactions().iter().enumerate() {
+            let hash = transaction_hashes
+                .as_ref()
+                .map(|tx_hashes| h256_to_felt(*tx_hashes.get(index).ok_or(StarknetRpcApiError::InternalServerError)?))
+                .unwrap_or_else(|| Ok(tx.compute_hash::<H>(chain_id.0.into(), false).0))?;
+            transactions.push(to_starknet_core_tx::<H>(tx.clone(), hash));
+        }
+
         let block_with_txs = BlockWithTxs {
             // TODO: Get status from block
             status: BlockStatus::AcceptedOnL2,
@@ -658,12 +693,7 @@ where
             new_root: block.header().global_state_root.into(),
             timestamp: block.header().block_timestamp,
             sequencer_address: Felt252Wrapper::from(block.header().sequencer_address).into(),
-            transactions: block
-                .transactions()
-                .iter()
-                .cloned()
-                .map(|tx| to_starknet_core_tx::<H>(tx, Felt252Wrapper(chain_id.0)))
-                .collect::<Vec<_>>(),
+            transactions,
         };
 
         Ok(MaybePendingBlockWithTxs::Block(block_with_txs))
@@ -731,7 +761,7 @@ where
         })?;
 
         let chain_id = self.chain_id()?;
-        let transactions = transactions.into_iter().map(|tx| to_starknet_core_tx::<H>(tx, chain_id.0.into())).collect();
+        let transactions = transactions.into_iter().map(|tx| to_starknet_core_tx::<H>(tx, chain_id.0)).collect();
 
         Ok(transactions)
     }
@@ -819,7 +849,7 @@ where
             .transactions()
             .iter()
             .find(|tx| tx.compute_hash::<H>(chain_id, false).0 == transaction_hash)
-            .map(|tx| to_starknet_core_tx::<H>(tx.clone(), chain_id));
+            .map(|tx| to_starknet_core_tx::<H>(tx.clone(), transaction_hash));
 
         find_tx.ok_or(StarknetRpcApiError::TxnHashNotFound.into())
     }
@@ -1008,5 +1038,15 @@ where
             Ok(starknet_error) => Err(starknet_error.into()),
             Err(_) => Err(StarknetRpcApiError::InternalServerError),
         },
+    }
+}
+
+fn h256_to_felt(h256: H256) -> Result<FieldElement, StarknetRpcApiError> {
+    match Felt252Wrapper::try_from(h256) {
+        Ok(felt) => Ok(felt.0),
+        Err(err) => {
+            error!("failed to convert H256 to FieldElement: {err}");
+            Err(StarknetRpcApiError::InternalServerError)
+        }
     }
 }

crates/primitives/transactions/src/to_starknet_core_transaction.rs
@@ -4,8 +4,6 @@ use mp_felt::Felt252Wrapper;
 use mp_hashers::HasherT;
 use starknet_crypto::FieldElement;
 
-use super::compute_hash::ComputeTransactionHash;
-
 fn cast_vec_of_felt_252_wrappers(data: Vec<Felt252Wrapper>) -> Vec<FieldElement> {
     // Non-copy but less dangerous than transmute
     // https://doc.rust-lang.org/std/mem/fn.transmute.html#alternatives
@@ -15,12 +13,10 @@ fn cast_vec_of_felt_252_wrappers(data: Vec<Felt252Wrapper>) -> Vec<FieldElement>
 
 pub fn to_starknet_core_tx<H: HasherT>(
     tx: super::Transaction,
-    chain_id: Felt252Wrapper,
+    transaction_hash: FieldElement,
 ) -> starknet_core::types::Transaction {
     match tx {
         super::Transaction::Declare(tx) => {
-            let tx_hash = tx.compute_hash::<H>(chain_id, false);
-
             let tx = match tx {
                 super::DeclareTransaction::V0(super::DeclareTransactionV0 {
                     max_fee,
@@ -29,7 +25,7 @@ pub fn to_starknet_core_tx<H: HasherT>(
                     class_hash,
                     sender_address,
                 }) => starknet_core::types::DeclareTransaction::V0(starknet_core::types::DeclareTransactionV0 {
-                    transaction_hash: tx_hash.0,
+                    transaction_hash,
                     max_fee: max_fee.into(),
                     signature: cast_vec_of_felt_252_wrappers(signature),
                     class_hash: class_hash.into(),
@@ -42,7 +38,7 @@ pub fn to_starknet_core_tx<H: HasherT>(
                     class_hash,
                     sender_address,
                 }) => starknet_core::types::DeclareTransaction::V1(starknet_core::types::DeclareTransactionV1 {
-                    transaction_hash: tx_hash.0,
+                    transaction_hash,
                     max_fee: max_fee.into(),
                     signature: cast_vec_of_felt_252_wrappers(signature),
                     nonce: nonce.into(),
@@ -57,7 +53,7 @@ pub fn to_starknet_core_tx<H: HasherT>(
                     sender_address,
                     compiled_class_hash,
                 }) => starknet_core::types::DeclareTransaction::V2(starknet_core::types::DeclareTransactionV2 {
-                    transaction_hash: tx_hash.0,
+                    transaction_hash,
                     max_fee: max_fee.into(),
                     signature: cast_vec_of_felt_252_wrappers(signature),
                     nonce: nonce.into(),
@@ -70,10 +66,8 @@ pub fn to_starknet_core_tx<H: HasherT>(
             starknet_core::types::Transaction::Declare(tx)
         }
         super::Transaction::DeployAccount(tx) => {
-            let tx_hash = tx.compute_hash::<H>(chain_id, false);
-
             let tx = starknet_core::types::DeployAccountTransaction {
-                transaction_hash: tx_hash.0,
+                transaction_hash,
                 max_fee: tx.max_fee.into(),
                 signature: cast_vec_of_felt_252_wrappers(tx.signature),
                 nonce: tx.nonce.into(),
@@ -85,8 +79,6 @@ pub fn to_starknet_core_tx<H: HasherT>(
             starknet_core::types::Transaction::DeployAccount(tx)
         }
         super::Transaction::Invoke(tx) => {
-            let tx_hash = tx.compute_hash::<H>(chain_id, false);
-
             let tx = match tx {
                 super::InvokeTransaction::V0(super::InvokeTransactionV0 {
                     max_fee,
@@ -95,7 +87,7 @@ pub fn to_starknet_core_tx<H: HasherT>(
                     entry_point_selector,
                     calldata,
                 }) => starknet_core::types::InvokeTransaction::V0(starknet_core::types::InvokeTransactionV0 {
-                    transaction_hash: tx_hash.0,
+                    transaction_hash,
                     max_fee: max_fee.into(),
                     signature: cast_vec_of_felt_252_wrappers(signature),
                     contract_address: contract_address.into(),
@@ -109,7 +101,7 @@ pub fn to_starknet_core_tx<H: HasherT>(
                     sender_address,
                     calldata,
                 }) => starknet_core::types::InvokeTransaction::V1(starknet_core::types::InvokeTransactionV1 {
-                    transaction_hash: tx_hash.0,
+                    transaction_hash,
                     max_fee: max_fee.into(),
                     signature: cast_vec_of_felt_252_wrappers(signature),
                     nonce: nonce.into(),
@@ -121,10 +113,8 @@ pub fn to_starknet_core_tx<H: HasherT>(
             starknet_core::types::Transaction::Invoke(tx)
         }
         super::Transaction::L1Handler(tx) => {
-            let tx_hash = tx.compute_hash::<H>(chain_id, false);
-
             let tx = starknet_core::types::L1HandlerTransaction {
-                transaction_hash: tx_hash.0,
+                transaction_hash,
                 version: 0,
                 nonce: tx.nonce,
                 contract_address: tx.contract_address.into(),

crates/client/db/src/lib.rs
@@ -19,7 +19,7 @@ mod meta_db;
 
 use std::marker::PhantomData;
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use da_db::DaDb;
 use mapping_db::MappingDb;
@@ -38,13 +38,22 @@ struct DatabaseSettings {
 }
 
 pub(crate) mod columns {
-    pub const NUM_COLUMNS: u32 = 5;
+    /// Total number of columns.
+    // ===== /!\ ===================================================================================
+    // MUST BE INCREMENTED WHEN A NEW COLUMN IN ADDED
+    // ===== /!\ ===================================================================================
+    pub const NUM_COLUMNS: u32 = 6;
 
     pub const META: u32 = 0;
     pub const BLOCK_MAPPING: u32 = 1;
     pub const TRANSACTION_MAPPING: u32 = 2;
     pub const SYNCED_MAPPING: u32 = 3;
     pub const DA: u32 = 4;
+    /// This column is used to map starknet block hashes to a list of transaction hashes that are
+    /// contained in the block.
+    ///
+    /// This column should only be accessed if the `--cache` flag is enabled.
+    pub const STARKNET_TRANSACTION_HASHES_CACHE: u32 = 5;
 }
 
 pub mod static_keys {
@@ -72,30 +81,33 @@ impl<B: BlockT> Backend<B> {
     /// Open the database
     ///
     /// The database will be created at db_config_dir.join(<db_type_name>)
-    pub fn open(database: &DatabaseSource, db_config_dir: &Path) -> Result<Self, String> {
-        Self::new(&DatabaseSettings {
-            source: match database {
-                DatabaseSource::RocksDb { .. } => {
-                    DatabaseSource::RocksDb { path: starknet_database_dir(db_config_dir, "rockdb"), cache_size: 0 }
-                }
-                DatabaseSource::ParityDb { .. } => {
-                    DatabaseSource::ParityDb { path: starknet_database_dir(db_config_dir, "paritydb") }
-                }
-                DatabaseSource::Auto { .. } => DatabaseSource::Auto {
-                    rocksdb_path: starknet_database_dir(db_config_dir, "rockdb"),
-                    paritydb_path: starknet_database_dir(db_config_dir, "paritydb"),
-                    cache_size: 0,
+    pub fn open(database: &DatabaseSource, db_config_dir: &Path, cache_more_things: bool) -> Result<Self, String> {
+        Self::new(
+            &DatabaseSettings {
+                source: match database {
+                    DatabaseSource::RocksDb { .. } => {
+                        DatabaseSource::RocksDb { path: starknet_database_dir(db_config_dir, "rockdb"), cache_size: 0 }
+                    }
+                    DatabaseSource::ParityDb { .. } => {
+                        DatabaseSource::ParityDb { path: starknet_database_dir(db_config_dir, "paritydb") }
+                    }
+                    DatabaseSource::Auto { .. } => DatabaseSource::Auto {
+                        rocksdb_path: starknet_database_dir(db_config_dir, "rockdb"),
+                        paritydb_path: starknet_database_dir(db_config_dir, "paritydb"),
+                        cache_size: 0,
+                    },
+                    _ => return Err("Supported db sources: `rocksdb` | `paritydb` | `auto`".to_string()),
                 },
-                _ => return Err("Supported db sources: `rocksdb` | `paritydb` | `auto`".to_string()),
             },
-        })
+            cache_more_things,
+        )
     }
 
-    fn new(config: &DatabaseSettings) -> Result<Self, String> {
+    fn new(config: &DatabaseSettings, cache_more_things: bool) -> Result<Self, String> {
         let db = db_opening_utils::open_database(config)?;
 
         Ok(Self {
-            mapping: Arc::new(MappingDb { db: db.clone(), write_lock: Arc::new(Mutex::new(())), _marker: PhantomData }),
+            mapping: Arc::new(MappingDb::new(db.clone(), cache_more_things)),
             meta: Arc::new(MetaDb { db: db.clone(), _marker: PhantomData }),
             da: Arc::new(DaDb { db: db.clone(), _marker: PhantomData }),
         })

crates/client/mapping-sync/src/lib.rs
@@ -55,7 +55,6 @@ impl<B: BlockT, C, BE, H> MappingSyncWorker<B, C, BE, H> {
         frontier_backend: Arc<mc_db::Backend<B>>,
         retry_times: usize,
         sync_from: <B::Header as HeaderT>::Number,
-        hasher: PhantomData<H>,
     ) -> Self {
         Self {
             import_notifications,
@@ -65,7 +64,7 @@ impl<B: BlockT, C, BE, H> MappingSyncWorker<B, C, BE, H> {
             client,
             substrate_backend,
             madara_backend: frontier_backend,
-            hasher,
+            hasher: PhantomData,
 
             have_next: true,
             retry_times,

crates/primitives/block/src/lib.rs
@@ -48,9 +48,14 @@ impl Block {
         &self.transactions
     }
 
-    /// Return a reference to all transaction hashes
-    pub fn transactions_hashes<H: HasherT>(&self, chain_id: Felt252Wrapper) -> Vec<Felt252Wrapper> {
-        self.transactions.iter().map(|tx| tx.compute_hash::<H>(chain_id, false)).collect()
+    /// Returns an iterator that iterates over all transaction hashes.
+    ///
+    /// Those transactions are computed using the given `chain_id`.
+    pub fn transactions_hashes<H: HasherT>(
+        &self,
+        chain_id: Felt252Wrapper,
+    ) -> impl '_ + Iterator<Item = Felt252Wrapper> {
+        self.transactions.iter().map(move |tx| tx.compute_hash::<H>(chain_id, false))
     }
 }
 

crates/client/db/src/mapping_db.rs
@@ -19,12 +19,19 @@ pub struct MappingCommitment<B: BlockT> {
 
 /// Allow interaction with the mapping db
 pub struct MappingDb<B: BlockT> {
-    pub(crate) db: Arc<dyn Database<DbHash>>,
-    pub(crate) write_lock: Arc<Mutex<()>>,
-    pub(crate) _marker: PhantomData<B>,
+    db: Arc<dyn Database<DbHash>>,
+    write_lock: Arc<Mutex<()>>,
+    /// Whether more information should be cached in the database.
+    cache_more_things: bool,
+    _marker: PhantomData<B>,
 }
 
 impl<B: BlockT> MappingDb<B> {
+    /// Creates a new instance of the mapping database.
+    pub fn new(db: Arc<dyn Database<DbHash>>, cache_more_things: bool) -> Self {
+        Self { db, write_lock: Arc::new(Mutex::new(())), cache_more_things, _marker: PhantomData }
+    }
+
     /// Check if the given block hash has already been processed
     pub fn is_synced(&self, block_hash: &B::Hash) -> Result<bool, String> {
         match self.db.get(crate::columns::SYNCED_MAPPING, &block_hash.encode()) {
@@ -85,11 +92,19 @@ impl<B: BlockT> MappingDb<B> {
 
         transaction.set(crate::columns::SYNCED_MAPPING, &commitment.block_hash.encode(), &true.encode());
 
-        for transaction_hash in commitment.starknet_transaction_hashes.into_iter() {
+        for transaction_hash in commitment.starknet_transaction_hashes.iter() {
             transaction.set(
                 crate::columns::TRANSACTION_MAPPING,
                 &transaction_hash.encode(),
                 &commitment.block_hash.encode(),
+            );
+        }
+
+        if self.cache_more_things {
+            transaction.set(
+                crate::columns::STARKNET_TRANSACTION_HASHES_CACHE,
+                &commitment.starknet_block_hash.encode(),
+                &commitment.starknet_transaction_hashes.encode(),
             );
         }
 
@@ -109,6 +124,32 @@ impl<B: BlockT> MappingDb<B> {
     pub fn block_hash_from_transaction_hash(&self, transaction_hash: H256) -> Result<Option<B::Hash>, String> {
         match self.db.get(crate::columns::TRANSACTION_MAPPING, &transaction_hash.encode()) {
             Some(raw) => Ok(Some(<B::Hash>::decode(&mut &raw[..]).map_err(|e| format!("{:?}", e))?)),
+            None => Ok(None),
+        }
+    }
+
+    /// Returns the list of transaction hashes for the given block hash.
+    ///
+    /// # Arguments
+    ///
+    /// * `starknet_hash` - the hash of the starknet block to search for.
+    ///
+    /// # Returns
+    ///
+    /// The list of transaction hashes.
+    ///
+    /// This function may return `None` for two separate reasons:
+    ///
+    /// - The cache is disabled.
+    /// - The provided `starknet_hash` is not present in the cache.
+    pub fn cached_transaction_hashes_from_block_hash(&self, starknet_hash: H256) -> Result<Option<Vec<H256>>, String> {
+        if !self.cache_more_things {
+            // The cache is not enabled, no need to even touch the database.
+            return Ok(None);
+        }
+
+        match self.db.get(crate::columns::STARKNET_TRANSACTION_HASHES_CACHE, &starknet_hash.encode()) {
+            Some(raw) => Ok(Some(Vec::<H256>::decode(&mut &raw[..]).map_err(|e| format!("{:?}", e))?)),
             None => Ok(None),
         }
     }

crates/node/src/command.rs
@@ -72,28 +72,28 @@ pub fn run() -> sc_cli::Result<()> {
         Some(Subcommand::CheckBlock(ref cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.async_run(|mut config| {
-                let (client, _, import_queue, task_manager, _) = service::new_chain_ops(&mut config)?;
+                let (client, _, import_queue, task_manager, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                 Ok((cmd.run(client, import_queue), task_manager))
             })
         }
         Some(Subcommand::ExportBlocks(ref cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.async_run(|mut config| {
-                let (client, _, _, task_manager, _) = service::new_chain_ops(&mut config)?;
+                let (client, _, _, task_manager, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                 Ok((cmd.run(client, config.database), task_manager))
             })
         }
         Some(Subcommand::ExportState(ref cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.async_run(|mut config| {
-                let (client, _, _, task_manager, _) = service::new_chain_ops(&mut config)?;
+                let (client, _, _, task_manager, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                 Ok((cmd.run(client, config.chain_spec), task_manager))
             })
         }
         Some(Subcommand::ImportBlocks(ref cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.async_run(|mut config| {
-                let (client, _, import_queue, task_manager, _) = service::new_chain_ops(&mut config)?;
+                let (client, _, import_queue, task_manager, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                 Ok((cmd.run(client, import_queue), task_manager))
             })
         }
@@ -104,7 +104,7 @@ pub fn run() -> sc_cli::Result<()> {
         Some(Subcommand::Revert(ref cmd)) => {
             let runner = cli.create_runner(cmd)?;
             runner.async_run(|mut config| {
-                let (client, backend, _, task_manager, _) = service::new_chain_ops(&mut config)?;
+                let (client, backend, _, task_manager, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                 let aux_revert = Box::new(|client, _, blocks| {
                     sc_consensus_grandpa::revert(client, blocks)?;
                     Ok(())
@@ -129,7 +129,7 @@ pub fn run() -> sc_cli::Result<()> {
                         cmd.run::<Block, service::ExecutorDispatch>(config)
                     }
                     BenchmarkCmd::Block(cmd) => {
-                        let (client, _, _, _, _) = service::new_chain_ops(&mut config)?;
+                        let (client, _, _, _, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                         cmd.run(client)
                     }
                     #[cfg(not(feature = "runtime-benchmarks"))]
@@ -145,13 +145,13 @@ pub fn run() -> sc_cli::Result<()> {
                         cmd.run(config, client, db, storage)
                     }
                     BenchmarkCmd::Overhead(cmd) => {
-                        let (client, _, _, _, _) = service::new_chain_ops(&mut config)?;
+                        let (client, _, _, _, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                         let ext_builder = RemarkBuilder::new(client.clone());
 
                         cmd.run(config, client, inherent_benchmark_data()?, Vec::new(), &ext_builder)
                     }
                     BenchmarkCmd::Extrinsic(cmd) => {
-                        let (client, _, _, _, _) = service::new_chain_ops(&mut config)?;
+                        let (client, _, _, _, _) = service::new_chain_ops(&mut config, cli.run.cache)?;
                         // Register the *Remark* builder.
                         let ext_factory = ExtrinsicFactory(vec![Box::new(RemarkBuilder::new(client.clone()))]);
 

crates/node/src/commands/run.rs
@@ -44,6 +44,14 @@ pub struct ExtendedRunCmd {
     /// Choose a supported DA Layer
     #[clap(long)]
     pub da_layer: Option<DaLayer>,
+
+    /// When enabled, more information about the blocks and their transaction is cached and stored
+    /// in the database.
+    ///
+    /// This may improve response times for RPCs that require that information, but it also
+    /// increases the memory footprint of the node.
+    #[clap(long)]
+    pub cache: bool,
 }
 
 impl ExtendedRunCmd {
@@ -78,10 +86,10 @@ pub fn run_node(mut cli: Cli) -> Result<()> {
             None
         }
     };
-
     runner.run_node_until_exit(|config| async move {
         let sealing = cli.run.sealing.map(Into::into).unwrap_or_default();
-        service::new_full(config, sealing, da_config).map_err(sc_cli::Error::Service)
+        let cache = cli.run.cache;
+        service::new_full(config, sealing, da_config, cache).map_err(sc_cli::Error::Service)
     })
 }
 

commit 2a4edbe2953059799eef7a79624809e788294a18

fix(cache-opt): fix typo introduced by #1197 (#1208)

crates/client/rpc/src/lib.rs
@@ -659,7 +659,7 @@ where
             .map(|tx_hashes| h256_to_felt(*tx_hashes.get(index as usize).ok_or(StarknetRpcApiError::InvalidTxnIndex)?))
             .unwrap_or_else(|| Ok(transaction.compute_hash::<H>(chain_id.0.into(), false).0))?;
 
-        Ok(to_starknet_core_tx::<H>(transaction.clone(), transaction_hash))
+        Ok(to_starknet_core_tx(transaction.clone(), transaction_hash))
     }
 
     /// Get block information with full transactions given the block id
@@ -681,7 +681,7 @@ where
                 .as_ref()
                 .map(|tx_hashes| h256_to_felt(*tx_hashes.get(index).ok_or(StarknetRpcApiError::InternalServerError)?))
                 .unwrap_or_else(|| Ok(tx.compute_hash::<H>(chain_id.0.into(), false).0))?;
-            transactions.push(to_starknet_core_tx::<H>(tx.clone(), hash));
+            transactions.push(to_starknet_core_tx(tx.clone(), hash));
         }
 
         let block_with_txs = BlockWithTxs {
@@ -761,8 +761,14 @@ where
         })?;
 
         let chain_id = self.chain_id()?;
-        let transactions = transactions.into_iter().map(|tx| to_starknet_core_tx::<H>(tx, chain_id.0)).collect();
 
+        let transactions = transactions
+            .into_iter()
+            .map(|tx| {
+                let hash = tx.compute_hash::<H>(chain_id.0.into(), false).into();
+                to_starknet_core_tx(tx, hash)
+            })
+            .collect();
         Ok(transactions)
     }
 
@@ -845,11 +851,19 @@ where
         let block = get_block_by_block_hash(self.client.as_ref(), substrate_block_hash).unwrap_or_default();
         let chain_id = self.chain_id()?.0.into();
 
-        let find_tx = block
-            .transactions()
-            .iter()
-            .find(|tx| tx.compute_hash::<H>(chain_id, false).0 == transaction_hash)
-            .map(|tx| to_starknet_core_tx::<H>(tx.clone(), transaction_hash));
+        let find_tx = if let Some(tx_hashes) = self.get_cached_transaction_hashes(block.header().hash::<H>().into()) {
+            tx_hashes
+                .into_iter()
+                .zip(block.transactions())
+                .find(|(tx_hash, _)| *tx_hash == Felt252Wrapper(transaction_hash).into())
+                .map(|(_, tx)| to_starknet_core_tx(tx.clone(), transaction_hash))
+        } else {
+            block
+                .transactions()
+                .iter()
+                .find(|tx| tx.compute_hash::<H>(chain_id, false).0 == transaction_hash)
+                .map(|tx| to_starknet_core_tx(tx.clone(), transaction_hash))
+        };
 
         find_tx.ok_or(StarknetRpcApiError::TxnHashNotFound.into())
     }

crates/primitives/transactions/src/to_starknet_core_transaction.rs
@@ -1,7 +1,6 @@
 use std::vec::Vec;
 
 use mp_felt::Felt252Wrapper;
-use mp_hashers::HasherT;
 use starknet_crypto::FieldElement;
 
 fn cast_vec_of_felt_252_wrappers(data: Vec<Felt252Wrapper>) -> Vec<FieldElement> {
@@ -11,7 +10,7 @@ fn cast_vec_of_felt_252_wrappers(data: Vec<Felt252Wrapper>) -> Vec<FieldElement>
     unsafe { alloc::vec::Vec::from_raw_parts(data.as_mut_ptr() as *mut FieldElement, data.len(), data.capacity()) }
 }
 
-pub fn to_starknet_core_tx<H: HasherT>(
+pub fn to_starknet_core_tx(
     tx: super::Transaction,
     transaction_hash: FieldElement,
 ) -> starknet_core::types::Transaction {