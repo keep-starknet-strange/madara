8903c8c2ebb6a4ade759a064adc99dca03643c4f
Juul-Mc-Goa <jules.magois.pro@proton.me>
2023-11-23 20:36:03 +0000

    revert `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
