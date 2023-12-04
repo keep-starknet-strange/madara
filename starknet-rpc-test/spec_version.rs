285d8e74258da3f643b70f78b9b4b5a4609380cb
antiyro <antiyro@gmail.com>
2023-12-04 17:50:06 +0100

    added ignore to future impl of spec version test

--- starknet-rpc-test/spec_version.rs
+++ starknet-rpc-test/spec_version.rs
@@ -7,6 +7,7 @@ use starknet_rpc_test::fixtures::{madara, ThreadSafeMadaraClient};
 
 #[rstest]
 #[tokio::test]
+#[ignore = "Waiting for starknet_providers::jsonrpc upgrade to v0.6.0"]
 async fn returns_hardcoded_spec_version(madara: &ThreadSafeMadaraClient) -> Result<(), anyhow::Error> {
     let rpc = madara.get_starknet_client().await;
 
