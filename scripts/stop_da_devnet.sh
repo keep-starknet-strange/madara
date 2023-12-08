1738ffd00c98f3612bf7929b3e5b967904bee63b
0xevolve <Artevolve@yahoo.com>
2023-12-08 15:30:50 +0100

    ci: start/stop da_devnet

---
+++ scripts/stop_da_devnet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# [ethereum, celestia, avail]
+DA_LAYER=$1
+
+if [ "$DA_LAYER" = "ethereum" ]; then
+    echo "Killing Anvil:"
+    ./zaun/scripts/sn-base-kill.sh target
+elif [ "$DA_LAYER" = "celestia" ]; then
+    # TODO: Kill Celestia
+elif [ "$DA_LAYER" = "avail" ]; then
+    # TODO: Kill Avail
+fi
