0decab2a3b17fcea1ad11a46e37543f684627636
0xevolve <Artevolve@yahoo.com>
2023-12-12 20:01:47 +0100

    feat: da layer test

--- da-test/state_diffs.rs
+++ da-test/state_diffs.rs
@@ -1,42 +1,58 @@
-extern crate starknet_rpc_test;
+extern crate da_test;
 
+use std::path::PathBuf;
 use std::vec;
 
-use assert_matches::assert_matches;
+use da_test::utils::get_da_client;
+use ethers::types::I256;
+use mc_data_availability::DaLayer;
 use rstest::rstest;
-use starknet_accounts::Account;
-use starknet_core::types::{BlockId, StarknetError};
 use starknet_ff::FieldElement;
-use starknet_providers::{MaybeUnknownErrorCode, Provider, ProviderError, StarknetErrorWithMessage};
-use starknet_rpc_test::constants::{ARGENT_CONTRACT_ADDRESS, FEE_TOKEN_ADDRESS, SIGNER_PRIVATE};
+use starknet_providers::Provider;
+use starknet_rpc_test::constants::ARGENT_CONTRACT_ADDRESS;
 use starknet_rpc_test::fixtures::{madara, ThreadSafeMadaraClient};
-use starknet_rpc_test::utils::{build_single_owner_account, read_erc20_balance, AccountActions, U256};
-use starknet_rpc_test::{SendTransactionError, Transaction};
+use starknet_rpc_test::utils::{build_single_owner_account, AccountActions};
+use starknet_rpc_test::Transaction;
 
 #[rstest]
+#[case(DaLayer::Celestia, "examples/da-confs/celestia.json")]
+#[case(DaLayer::Ethereum, "examples/da-confs/ethereum.json")]
+#[case(DaLayer::Avail, "examples/da-confs/avail.json")]
 #[tokio::test]
-async fn publishes_to_da_layer(madara: &ThreadSafeMadaraClient) -> Result<(), anyhow::Error> {
+async fn publish_to_da_layer(
+    madara: &ThreadSafeMadaraClient,
+    #[case] da_layer: DaLayer,
+    #[case] da_path: PathBuf,
+) -> Result<(), anyhow::Error> {
     let rpc = madara.get_starknet_client().await;
 
-    let txs = {
+    let (txs, block_number) = {
         let mut madara_write_lock = madara.write().await;
         // using incorrect private key to generate the wrong signature
         let account = build_single_owner_account(&rpc, "0x1234", ARGENT_CONTRACT_ADDRESS, true);
 
-        madara_write_lock
+        let txs = madara_write_lock
             .create_block_with_txs(vec![Transaction::Execution(account.transfer_tokens(
                 FieldElement::from_hex_be("0x123").unwrap(),
                 FieldElement::ONE,
                 None,
             ))])
-            .await?
+            .await?;
+        let block_number = rpc.block_number().await?;
+
+        (txs, block_number)
     };
 
     assert_eq!(txs.len(), 1);
 
-    let tx = &txs[0];
+    let _tx = &txs[0];
+
+    let da_client = get_da_client(da_layer, da_path);
 
     // Check the state diff that has been published to the DA layer
+    let published_block_number = da_client.last_published_state().await?;
+
+    assert_eq!(published_block_number, I256::from(block_number));
 
     Ok(())
 }
