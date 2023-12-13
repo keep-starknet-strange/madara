e1b1766e24e3ef4f9e116c0e230dc81a9c602b88
Evolve <artevolve@yahoo.com>
2023-12-13 16:34:05 +0100

    fix: confs paths

--- da-test/src/constants.rs
+++ da-test/src/constants.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use lazy_static::lazy_static;
 
 lazy_static! {
-    pub static ref ETHEREUM_DA_CONFIG: PathBuf = PathBuf::from("examples/da-confs/ethereum.json");
-    pub static ref CELESTIA_DA_CONFIG: PathBuf = PathBuf::from("examples/da-confs/celestia.json");
-    pub static ref AVAIL_DA_CONFIG: PathBuf = PathBuf::from("examples/da-confs/avail.json");
+    pub static ref ETHEREUM_DA_CONFIG: PathBuf = PathBuf::from("../examples/da-confs/ethereum.json");
+    pub static ref CELESTIA_DA_CONFIG: PathBuf = PathBuf::from("../examples/da-confs/celestia.json");
+    pub static ref AVAIL_DA_CONFIG: PathBuf = PathBuf::from("../examples/da-confs/avail.json");
 }
