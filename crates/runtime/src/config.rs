d4d2764dd3d5c01cc65e53f166b872deb877d1f2
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-01-04 15:06:37 +0530

    revert block time

--- crates/runtime/src/config.rs
+++ crates/runtime/src/config.rs
@@ -26,7 +26,7 @@ use crate::{BlockNumber, RUNTIME_API_VERSIONS};
 ///
 /// Change this to adjust the block time.
 /// a.k.a `BLOCK_TIME`
-pub const MILLISECS_PER_BLOCK: u64 = 60000;
+pub const MILLISECS_PER_BLOCK: u64 = 6000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
