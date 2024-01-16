cceee0e46892d1633596a4dee5c55e14f895a65d
apoorvsadana <apoorvsadana@users.noreply.github.com>
2024-01-16 07:42:46 +0000

    deploy: 8c285b45101dd20736233408f37d91ab87a995cd

--- pallet_starknet/sidebar-items.js
+++ pallet_starknet/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["ETHEREUM_CONSENSUS_RPC","ETHEREUM_EXECUTION_RPC"],"externcrate":["alloc"],"macro":["log"],"mod":["blockifier_state_adapter","execution_config","genesis_loader","pallet","transaction_validation","types"]};
+window.SIDEBAR_ITEMS = {"constant":["ETHEREUM_CONSENSUS_RPC","ETHEREUM_EXECUTION_RPC","SN_OS_CONFIG_HASH_VERSION"],"externcrate":["alloc"],"macro":["log"],"mod":["blockifier_state_adapter","execution_config","genesis_loader","pallet","transaction_validation","types"]};
