dbedad429dcf9ea0c9e0efc5243846a31da9411b
tdelabro <tdelabro@users.noreply.github.com>
2024-02-05 15:21:32 +0000

    deploy: 7900c0b884ee3fb978711ff65b19e98f0c143c03

--- mc_data_availability/utils/sidebar-items.js
+++ mc_data_availability/utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["bytes_to_felt","bytes_to_key","da_word","get_bytes_from_state_diff","get_valid_url","is_valid_http_endpoint","is_valid_ws_endpoint","safe_split","state_diff_to_calldata"]};
+window.SIDEBAR_ITEMS = {"fn":["block_data_to_calldata","bytes_to_felt","bytes_to_key","da_word","get_bytes_from_state_diff","get_valid_url","is_valid_http_endpoint","is_valid_ws_endpoint","safe_split"]};
