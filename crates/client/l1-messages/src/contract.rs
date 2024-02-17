34bdd71571552c74a48f5d46cd0cae3e34f4d6cc
Anshal Shukla <shukla.anshal85@gmail.com>
2024-02-17 16:01:17 +0530

    add: from address in l1 message calldata

--- crates/client/l1-messages/src/contract.rs
+++ crates/client/l1-messages/src/contract.rs
@@ -1,5 +1,6 @@
 use mp_felt::{Felt252Wrapper, Felt252WrapperError};
 use mp_transactions::HandleL1MessageTransaction;
+use starknet_api::hash::StarkFelt;
 use starknet_core_contract_client::interfaces::LogMessageToL2Filter;
 
 #[derive(thiserror::Error, Debug, PartialEq)]
@@ -11,6 +12,8 @@ pub enum L1EventToTransactionError {
     InvalidContractAddress(Felt252WrapperError),
     #[error("Failed to convert Entrypoint Selector from L1 Event: `{0}`")]
     InvalidEntryPointSelector(Felt252WrapperError),
+    #[error("Failed to convert From Address from L1 Event: `{0}`")]
+    InvalidFromAddress(Felt252WrapperError),
     #[error("Failed to convert Nonce param from L1 Event: `{0}`")]
     InvalidNonce(Felt252WrapperError),
 }
@@ -18,6 +21,10 @@ pub enum L1EventToTransactionError {
 pub fn parse_handle_l1_message_transaction(
     event: LogMessageToL2Filter,
 ) -> Result<HandleL1MessageTransaction, L1EventToTransactionError> {
+    // L1 from address.
+    let from_address = Felt252Wrapper::try_from(sp_core::U256::from_big_endian(event.from_address.as_bytes()))
+        .map_err(L1EventToTransactionError::InvalidFromAddress)?;
+
     // L2 contract to call.
     let contract_address = Felt252Wrapper::try_from(sp_core::U256(event.to_address.0))
         .map_err(L1EventToTransactionError::InvalidContractAddress)?;
@@ -32,12 +39,15 @@ pub fn parse_handle_l1_message_transaction(
         .try_into()
         .map_err(L1EventToTransactionError::InvalidNonce)?;
 
-    let calldata: Vec<Felt252Wrapper> = event
-        .payload
-        .iter()
-        .map(|param| Felt252Wrapper::try_from(sp_core::U256(param.0)))
-        .collect::<Result<Vec<Felt252Wrapper>, Felt252WrapperError>>()
-        .map_err(L1EventToTransactionError::InvalidCalldata)?;
+    let mut calldata: Vec<Felt252Wrapper> = vec![from_address];
+    calldata.extend(
+        event
+            .payload
+            .iter()
+            .map(|param| Felt252Wrapper::try_from(sp_core::U256(param.0)))
+            .collect::<Result<Vec<Felt252Wrapper>, Felt252WrapperError>>()
+            .map_err(L1EventToTransactionError::InvalidCalldata)?,
+    );
 
     Ok(HandleL1MessageTransaction { nonce, contract_address, entry_point_selector, calldata })
 }
