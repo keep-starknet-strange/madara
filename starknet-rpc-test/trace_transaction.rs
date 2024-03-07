d9b569d3c92bb019cde36f6bfc6f58536e4c8d00
0xTrinityy <trinity0x@protonmail.com>
2024-03-07 14:57:42 +0100

    feat: :bug: trying to fix test bug

--- starknet-rpc-test/trace_transaction.rs
+++ starknet-rpc-test/trace_transaction.rs
@@ -55,7 +55,7 @@ async fn works_with_correct_transaction(madara: &ThreadSafeMadaraClient) -> Resu
     };
 
     // included in block
-    let included_tx = rpc.get_transaction_by_block_id_and_index(BlockId::Number(block_number), 0).await?;
+    let included_tx = rpc.get_transaction_by_block_id_and_index(BlockId::Number(block_number), 1).await?;
     let included_tx_hash = included_tx.transaction_hash();
 
     let trace = rpc.trace_transaction(included_tx_hash).await?;
