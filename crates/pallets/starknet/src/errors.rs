f08e02b8ff98540ef226f109671f1e9ea6d05722
User <53880692+hhamud@users.noreply.github.com>
2024-04-07 06:53:15 +0100

    fix: dont use scalec coding

--- crates/pallets/starknet/src/errors.rs
+++ crates/pallets/starknet/src/errors.rs
@@ -1,7 +1,7 @@
 use blockifier::execution::errors::{EntryPointExecutionError, PreExecutionError};
 use blockifier::transaction::errors::TransactionExecutionError;
 use frame_support::traits::PalletError;
-use parity_scale_codec::{Decode, Encode};
+use parity_scale_codec::{Decode, Encode, Output};
 use scale_info::build::{Fields, Variants};
 use scale_info::{Path, Type, TypeInfo};
 
@@ -34,50 +34,69 @@ impl TypeInfo for BlockifierErrors {
 // https://github.com/paritytech/parity-scale-codec
 impl Decode for BlockifierErrors {
     fn decode<I: parity_scale_codec::Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
-        todo!()
+        // read the first byte to decode the index,
+        // the index will decide the type of error
+        let index = input.read_byte()?;
+
+        // convert bytes into string and then parse the string back into the error
+        // create a new error depending on the string received
+        let mut buffer: Vec<u8> = Vec::new();
+
+        //skip the first byte
+        input.read(&mut buffer)?;
+
+        let message = String::from_utf8(buffer).unwrap();
+
+        //convert the vector bytes back into blockifier error
+        // create a mini parser that decodes
+
+        Ok(match index {
+            0 => BlockifierErrors::EntryPointExecutionError(message),
+            1 => BlockifierErrors::PreExecutionError(message),
+            2 => BlockifierErrors::TransactionExecutionError(message),
+            _ => return Err("Invalid variant index".into()),
+        })
     }
 }
 
 // encode errors into a vector of bytes
 impl Encode for BlockifierErrors {
     fn size_hint(&self) -> usize {
-        match self {
-            BlockifierErrors::EntryPointExecutionError(_) => 0,
-            BlockifierErrors::PreExecutionError(_) => 0,
-            BlockifierErrors::TransactionExecutionError(_) => 0,
-        }
+        // an extra byte for padding
+        self.encode().len() + 1
     }
 
-    fn encode_to<T: parity_scale_codec::Output + ?Sized>(&self, dest: &mut T) {
-        match self {
-            BlockifierErrors::EntryPointExecutionError(_) => todo!(),
-            BlockifierErrors::PreExecutionError(_) => todo!(),
-            BlockifierErrors::TransactionExecutionError(_) => todo!(),
-        }
+    fn encode_to<T: Output + ?Sized>(&self, dest: &mut T) {
+        let index: u8 = match self {
+            BlockifierErrors::EntryPointExecutionError(_) => 0,
+            BlockifierErrors::PreExecutionError(_) => 1,
+            BlockifierErrors::TransactionExecutionError(_) => 2,
+        };
+
+        dest.push_byte(index);
+
+        let encoded_message = match self {
+            BlockifierErrors::EntryPointExecutionError(err) => err.to_string().as_bytes(),
+            BlockifierErrors::PreExecutionError(err) => err.to_string().as_bytes(),
+            BlockifierErrors::TransactionExecutionError(err) => err.to_string().as_bytes(),
+        };
+
+        dest.write(encoded_message)
     }
 
     fn encode(&self) -> Vec<u8> {
-        match self {
-            BlockifierErrors::EntryPointExecutionError(_) => todo!(),
-            BlockifierErrors::PreExecutionError(_) => todo!(),
-            BlockifierErrors::TransactionExecutionError(_) => todo!(),
-        }
+        let mut encoded = Vec::with_capacity(self.size_hint());
+        self.encode_to(&mut encoded);
+        encoded
     }
 
     fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
-        match self {
-            BlockifierErrors::EntryPointExecutionError(_) => todo!(),
-            BlockifierErrors::PreExecutionError(_) => todo!(),
-            BlockifierErrors::TransactionExecutionError(_) => todo!(),
-        }
+        let mut encoded = Vec::with_capacity(self.size_hint());
+        self.encode_to(&mut encoded);
+        f(&encoded)
     }
-
     fn encoded_size(&self) -> usize {
-        match self {
-            BlockifierErrors::EntryPointExecutionError(_) => todo!(),
-            BlockifierErrors::PreExecutionError(_) => todo!(),
-            BlockifierErrors::TransactionExecutionError(_) => todo!(),
-        }
+        self.size_hint()
     }
 }
 
