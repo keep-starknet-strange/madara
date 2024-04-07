93b051626ce8f48cf18603a0b4cfa8ce45ced35d
fishseabowl <xuefei.han@domichain.io>
2024-04-07 10:17:14 -0600

    Compare estimeate_fee with actual_fee in TransactionReceipt

--- starknet-rpc-test/estimate_fee.rs
+++ starknet-rpc-test/estimate_fee.rs
@@ -2,13 +2,19 @@ extern crate starknet_rpc_test;
 
 use assert_matches::assert_matches;
 use rstest::rstest;
-use starknet_core::types::{BlockId, BlockTag, BroadcastedInvokeTransaction, BroadcastedTransaction, StarknetError};
+use starknet_accounts::{Call, ConnectedAccount};
+use starknet_core::types::{
+    BlockId, BlockTag, BroadcastedInvokeTransaction, BroadcastedTransaction, MaybePendingTransactionReceipt,
+    StarknetError, TransactionReceipt,
+};
 use starknet_core::utils::get_selector_from_name;
 use starknet_ff::FieldElement;
 use starknet_providers::ProviderError::StarknetError as StarknetProviderError;
 use starknet_providers::{MaybeUnknownErrorCode, Provider, StarknetErrorWithMessage};
-use starknet_rpc_test::constants::{ACCOUNT_CONTRACT, TEST_CONTRACT_ADDRESS};
+use starknet_rpc_test::constants::{ACCOUNT_CONTRACT, ARGENT_CONTRACT_ADDRESS, SIGNER_PRIVATE, TEST_CONTRACT_ADDRESS};
 use starknet_rpc_test::fixtures::{madara, ThreadSafeMadaraClient};
+use starknet_rpc_test::utils::{build_single_owner_account, AccountActions};
+use tokio::time;
 
 #[rstest]
 #[tokio::test]
@@ -100,35 +106,39 @@ async fn works_ok(madara: &ThreadSafeMadaraClient) -> Result<(), anyhow::Error> 
     let invoke_transaction = BroadcastedTransaction::Invoke(tx.clone());
 
     let invoke_transaction_2 =
-        BroadcastedTransaction::Invoke(BroadcastedInvokeTransaction { nonce: FieldElement::ONE, ..tx });
+        BroadcastedTransaction::Invoke(BroadcastedInvokeTransaction { nonce: FieldElement::ONE, ..tx.clone() });
 
     let estimates =
         rpc.estimate_fee(&vec![invoke_transaction, invoke_transaction_2], BlockId::Tag(BlockTag::Latest)).await?;
 
-    let tx = BroadcastedInvokeTransaction {
-        max_fee: FieldElement::from(210u16),
-        signature: vec![],
-        nonce: FieldElement::ZERO,
-        sender_address,
-        calldata,
-        is_query: false,
-    };
-
-    let invoke_transaction = BroadcastedTransaction::Invoke(tx.clone());
-
-    let invoke_transaction_2 =
-        BroadcastedTransaction::Invoke(BroadcastedInvokeTransaction { nonce: FieldElement::ONE, ..tx });
-
-    let simulations = rpc
-        .simulate_transactions(BlockId::Tag(BlockTag::Latest), &[invoke_transaction, invoke_transaction_2], [])
-        .await?;
-
     assert_eq!(estimates.len(), 2);
-    assert_eq!(estimates[0].overall_fee, simulations[0].fee_estimation.overall_fee);
-    assert_eq!(estimates[1].overall_fee, simulations[1].fee_estimation.overall_fee);
-    // https://starkscan.co/block/5
-    assert_eq!(estimates[0].gas_consumed, simulations[0].fee_estimation.gas_consumed);
-    assert_eq!(estimates[1].gas_consumed, simulations[1].fee_estimation.gas_consumed);
+    assert_eq!(estimates[0].gas_consumed, 0);
+    assert_eq!(estimates[1].gas_consumed, 0);
+
+    let funding_account = build_single_owner_account(&rpc, SIGNER_PRIVATE, ARGENT_CONTRACT_ADDRESS, true);
+    let nonce = funding_account.get_nonce().await?;
+    let max_fee = FieldElement::from(1000u16);
+    let calls = vec![Call {
+        to: FieldElement::from_hex_be(TEST_CONTRACT_ADDRESS).unwrap(),
+        selector: get_selector_from_name("sqrt").unwrap(),
+        calldata: vec![FieldElement::from(81u8)],
+    }];
+
+    let tx = funding_account.prepare_invoke(calls, nonce, max_fee, false).await;
+
+    let invoke_transaction_result = rpc.add_invoke_transaction(tx.clone()).await?;
+
+    time::sleep(std::time::Duration::from_secs(20)).await;
+
+    let invoke_tx_receipt = rpc.get_transaction_receipt(invoke_transaction_result.transaction_hash).await?;
+
+    match invoke_tx_receipt {
+        MaybePendingTransactionReceipt::Receipt(TransactionReceipt::Invoke(receipt)) => {
+            assert_eq!(FieldElement::from(estimates[0].overall_fee), receipt.actual_fee);
+        }
+
+        _ => panic!("expected invoke transaction receipt"),
+    }
 
     Ok(())
 }
