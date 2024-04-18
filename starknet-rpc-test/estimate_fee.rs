717e3d1c56445a7479d14bdb87fe25ed21ca8663
fishseabowl <xuefei.han@domichain.io>
2024-04-18 09:45:14 -0700

    fix fmt issue

--- starknet-rpc-test/estimate_fee.rs
+++ starknet-rpc-test/estimate_fee.rs
@@ -1,5 +1,6 @@
 extern crate starknet_rpc_test;
 use std::time::Duration;
+
 use assert_matches::assert_matches;
 use rstest::rstest;
 use starknet_accounts::{Call, ConnectedAccount};
@@ -15,8 +16,6 @@ use starknet_rpc_test::constants::{ACCOUNT_CONTRACT, ARGENT_CONTRACT_ADDRESS, SI
 use starknet_rpc_test::fixtures::{madara, ThreadSafeMadaraClient};
 use starknet_rpc_test::utils::{build_single_owner_account, AccountActions};
 use tokio::time;
-use starknet_test_utils::constants::{ACCOUNT_CONTRACT, TEST_CONTRACT_ADDRESS};
-use starknet_test_utils::fixtures::{madara, ThreadSafeMadaraClient};
 
 #[rstest]
 #[tokio::test]
