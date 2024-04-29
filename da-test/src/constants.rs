410db0ec2e711d55c7a5c84cfeaaf131ea1c15e7
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2024-04-29 17:11:55 -0300

    remove all da & settlement related content

--- da-test/src/constants.rs
+++
@@ -1,3 +0,0 @@
-pub const ETHEREUM_DA_CONFIG: &str = include_str!("../../examples/da-confs/ethereum.json");
-pub const CELESTIA_DA_CONFIG: &str = include_str!("../../examples/da-confs/celestia.json");
-pub const AVAIL_DA_CONFIG: &str = include_str!("../../examples/da-confs/avail.json");
