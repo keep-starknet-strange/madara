97acf38b2c4d2e5c7789110a59e32ce9604266ee
fishseabowl <xuefei.han@domichain.io>
2024-05-29 09:27:18 -0700

    Fix merge conflicts

--- starknet-rpc-test/estimate_fee.rs
+++ starknet-rpc-test/estimate_fee.rs
@@ -1,5 +1,4 @@
 extern crate starknet_rpc_test;
-use std::time::Duration;
 use anyhow::Error;
 use assert_matches::assert_matches;
 use rstest::rstest;
@@ -11,11 +10,9 @@ use starknet_core::utils::get_selector_from_name;
 use starknet_ff::FieldElement;
 use starknet_providers::Provider;
 use starknet_providers::ProviderError::StarknetError as StarknetProviderError;
-use starknet_providers::{MaybeUnknownErrorCode, Provider, StarknetErrorWithMessage};
-use starknet_rpc_test::constants::{ACCOUNT_CONTRACT, ARGENT_CONTRACT_ADDRESS, SIGNER_PRIVATE, TEST_CONTRACT_ADDRESS};
+use starknet_rpc_test::constants::{ACCOUNT_CONTRACT_ADDRESS, ARGENT_CONTRACT_ADDRESS, MULTIPLY_TEST_CONTRACT_ADDRESS, SIGNER_PRIVATE, TEST_CONTRACT_ADDRESS};
 use starknet_rpc_test::fixtures::{madara, ThreadSafeMadaraClient};
-use starknet_rpc_test::utils::{build_single_owner_account, AccountActions};
-use tokio::time;
+use starknet_rpc_test::utils::{build_single_owner_account, get_transaction_receipt, is_good_error_code};
 
 #[rstest]
 #[tokio::test]
@@ -88,14 +85,6 @@ async fn fail_if_one_txn_cannot_be_executed(madara: &ThreadSafeMadaraClient) -> 
 #[tokio::test]
 async fn works_ok(madara: &ThreadSafeMadaraClient) -> Result<(), anyhow::Error> {
     let rpc = madara.get_starknet_client().await;
-    let funding_account = build_single_owner_account(&rpc, SIGNER_PRIVATE, ARGENT_CONTRACT_ADDRESS, true);
-    let nonce = funding_account.get_nonce().await?;
-    let max_fee = FieldElement::from(1000u16);
-    let calls = vec![Call {
-        to: FieldElement::from_hex_be(TEST_CONTRACT_ADDRESS).unwrap(),
-        selector: get_selector_from_name("sqrt").unwrap(),
-        calldata: vec![FieldElement::from(81u8)],
-    }];
 
     let sender_address = FieldElement::from_hex_be(ACCOUNT_CONTRACT_ADDRESS).unwrap();
     let nonce = rpc.get_nonce(BlockId::Tag(BlockTag::Latest), sender_address).await.unwrap();
